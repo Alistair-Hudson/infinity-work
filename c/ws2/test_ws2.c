3bb27f3a451e2fe617a72f9f90984e167ed9be9a
alistair.hudson <alistair.hudson@gmal.com>
2020-01-14 08:28:28 +0200

    Commit files that I failed to do so last night

--- c/ws2/test_ws2.c
+++ c/ws2/test_ws2.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "ws2.c"
 
 void LenTest(char *test_str)
 {
+	printf("%s\n", test_str);
 	int test_len = Strlen(test_str);
 	int exp_len = strlen(test_str);
 	if(test_len == exp_len)
 	{
-		printf("Length test passed");
+		printf("Length test passed\n");
 	}else
 	{
-		printf("Length test failed");
+		printf("Length test failed\n");
+		printf("test = %d, exp = %d\n", test_len, exp_len);
 	}
 }
 
 void CmpTest(char *test_str1, char *test_str2)
 {
+	printf("Test = %s, Exp = %s\n", test_str1, test_str2);
 	int test_res = Strcmp(test_str1, test_str2);
 	int exp_res = strcmp(test_str1, test_str2);
 	if(test_res == exp_res)
 	{
-		printf("Compare test passed");
+		printf("Compare test passed\n");
 	}else
 	{
-		printf("Compare test failed");
+		printf("Compare test failed\n");
+		printf("test = %d, exp = %d\n", test_res, exp_res);
 	}
 }
 
@@ -32,12 +37,14 @@ void CpyTest(char *test_str1, char *test_str2, char *test_str3)
 {
 	Strcpy(test_str1, test_str3);
 	strcpy(test_str2, test_str3);
-	if(test_str1 == test_str2)
+	if(strcmp(test_str1, test_str2) == 0)
 	{
-		printf("Copy test passed");
+		printf("Copy test passed\n");
+		printf("Test = %s, exp = %s\n", test_str1, test_str2);
 	}else
 	{
-		printf("Copy test failed");
+		printf("Copy test failed\n");
+		printf("Test = %s, exp = %s\n", test_str1, test_str2);
 	}
 }
 
@@ -45,12 +52,14 @@ void CpynTest(char *test_str1, char *test_str2, char *test_str3, int n)
 {
 	Strncpy(test_str1, test_str3, n);
 	strncpy(test_str2, test_str3, n);
-	if(test_str1 == test_str2)
+	if(strcmp(test_str1, test_str2) == 0)
 	{
-		printf("Copy n test passed");
+		printf("Copy n test passed\n");
+		printf("Test = %s, exp = %s\n", test_str1, test_str2);
 	}else
 	{
-		printf("Copy n test failed");
+		printf("Copy n test failed\n");
+		printf("Test = %s, exp = %s\n", test_str1, test_str2);
 	}
 }
 
@@ -60,10 +69,11 @@ void CaseCmpTest(char *test_str1, char *test_str2)
 	int exp_res = strcasecmp(test_str1, test_str2);
 	if(test_res == exp_res)
 	{
-		printf("Case cmp test passed");
+		printf("Case cmp test passed\n");
 	}else
 	{
-		printf("Case cmp test failed");
+		printf("Case cmp test failed\n");
+		printf("test = %d, exp = %d\n", test_res, exp_res);
 	}
 }
 
@@ -73,10 +83,11 @@ void ChrTest(char *test_str, int ch)
 	char *exp_ret = strchr(test_str, ch);
 	if(test_ret == exp_ret)
 	{
-		printf("Chr test passed");
+		printf("Chr test passed\n");
 	}else
 	{
-		printf("Chr test failed");
+		printf("Chr test failed\n");
+		printf("test = %s, exp = %s\n", test_ret, exp_ret);
 	}
 }
 
@@ -84,24 +95,46 @@ void DupTest(char *test_str)
 {
 	char *test_ret = Strdup(test_str);
 	char *exp_ret = strdup(test_str);
-	if(test_ret == exp_ret)
+	if(strcmp(test_ret, exp_ret) == 0)
 	{
-		printf("Dup test passed");
+		printf("Dup test passed\n");
+		printf("test = %s, exp = %s\n", test_ret, exp_ret);
 	}else
 	{
-		printf("Dup test failed");
+		printf("Dup test failed\n");
+		printf("test = %s, exp = %s\n", test_ret, exp_ret);
 	}
 }
 
 
-void CatTest()
+void CatTest(char *test_base, char *exp_base, char *append)
 {
-
+	Strcat(test_base, append);
+	strcat(exp_base, append);
+	if(strcmp(test_base, exp_base) == 0)
+	{
+		printf("Cat test passed\n");
+		printf("test = %s, exp = %s\n", test_base, exp_base);
+	}else
+	{
+		printf("Cat test failed\n");
+		printf("test = %s, exp = %s\n", test_base, exp_base);
+	}
 }
 
-void CatnTest()
+void CatnTest(char *test_base, char *exp_base, char *append, int n)
 {
-
+	Strncat(test_base, append, n);
+	strncat(exp_base, append, n);
+	if(strcmp(test_base, exp_base) == 0)
+	{
+		printf("Cat n test passed\n");
+		printf("test = %s, exp = %s\n", test_base, exp_base);
+	}else
+	{
+		printf("Cat n test failed\n");
+		printf("test = %s, exp = %s\n", test_base, exp_base);
+	}
 }
 
 void StrTest(char *test_hay, char *test_needle)
@@ -110,40 +143,67 @@ void StrTest(char *test_hay, char *test_needle)
 	char *exp_ret = strstr(test_hay, test_needle);
 	if(test_ret == exp_ret)
 	{
-		printf("Str test passed");
+		printf("Str test passed\n");
 	}else
 	{
-		printf("Str test failed");
+		printf("Str test failed\n");
+		printf("test = %s, exp = %s\n", test_ret, exp_ret);
+		
 	}
 }
 
 void SpnTest(char *test_str, char *test_span)
 {
+	printf("Span = %s\n", test_span);
 	int test_len = Strspn(test_str, test_span);
 	int exp_len = strspn(test_str, test_span);
 	if(test_len == exp_len)
 	{
-		printf("Spn test passed");
+		printf("Spn test passed\n");
 	}else
 	{
-		printf("Spn test failed");
+		printf("Spn test failed\n");
+		printf("test = %d, exp = %d\n", test_len, exp_len);
 	}
 }
 
-void TokTest()
+void TokTest(char *test_str, char *delin)
 {
-
+	int round = 0;
+	char *test_ret = Strtok(test_str, delin);
+	char *exp_ret = strtok(test_str, delin);
+	while((test_ret != NULL) && (exp_ret != NULL))
+	{
+		++round;
+		test_ret = Strtok(NULL, delin);
+		exp_ret = strtok(NULL, delin);
+	}
+	if(test_ret == exp_ret)
+	{
+		printf("Tok test passed\n");
+	}else
+	{
+		printf("Tok test failed\n");
+		printf("test = %s, exp = %s\n, round = %d", test_ret, exp_ret, round);
+	}
 }
 
 void PalindromeTest()
-{
+{	
+	printf("Palindrome Test:\n");
 	int total_pass = 0;
-	if(IsPalindrome("HANNAH") == 1){++total_pass;}
-	if(IsPalindrome("Hannah") == 1){++total_pass;}
-	if(IsPalindrome("ABC123") == 0){++total_pass;}
-	if(IsPalindrome("12321") == 1){++total_pass;}
-	if(IsPalindrome("!@# #@!") == 1){++total_pass;}
-	printf("Palindrome test passed %d times", total_pass);
+	char test1[] ="HANNAH";
+	char test2[] ="Hannah";
+	char test3[] ="ABCdef123";
+	char test4[] ="12321";
+	char test5[] ="!@# #@!";
+	if(IsPalindrome(test1) == 1){++total_pass;}
+	if(IsPalindrome(test2) == 1){++total_pass;}
+	if(IsPalindrome(test3) == 0){++total_pass;}
+	if(IsPalindrome(test4) == 1){++total_pass;}
+	if(IsPalindrome(test5) == 1){++total_pass;}
+	printf(" passed %d times\n", total_pass);
+	if(total_pass == 5){printf("Complete pass\n");}
 }
 
 void Boom7Test()
@@ -159,12 +219,12 @@ void SwapTest()
 	int *add2 = &num2;
 	Swap(&add1, &add2);
 
-	if((num1 == add2) && (num2 == add1)
+	if((num1 == *add2) && (num2 == *add1))
 	{
-		printf("Swap test passed");
+		printf("Swap test passed\n");
 	}else
 	{
-		printf("Swap test failed");
+		printf("Swap test failed\n");
 	}
 }
 
@@ -173,11 +233,71 @@ void SumTest()
 	int total_pass = 0;
 	char *res;
 	res = StringSum("1234", "851");
-	if(res == "2085"){++total_pass;}
+	//if(res == "2085"){++total_pass;}
 	printf("Sum test passed %d times", total_pass);
 }
 
-void main()
+int main()
 {
+	/*Testing variables*/
+	/*Length test*/
+	char test_len[] = "Length is 12";
+	/*Cmp tests*/
+	char test_cmp[] = "Hello";
+	char exp_cmp_equ[] = "Hello";
+	char exp_cmp_gtr[] = "hello";
+	char exp_cmp_lss[] = "HELLO";
+	char exp_cmp_lng[] = "Hello+";
+	/*Copy, copy n, and Dup tests*/
+	char cpy[] = "this is the copy";
+	char test[] = "this is the test";
+	char exp[] = "this is the expected";
+	char n_test[] = "12345";
+	char n_exp[] = "12345";
+	char n_test_lng[] = "123456";
+	char n_exp_lng[] = "123456";
+	int n = 5;
+	/*Chr test*/
+	int ch = ' ';
+	/*Cat and Cat n tests*/
+	char base[15] = "hello";
+	char append[] = " world";
+	/*Str and Spn tests*/
+	char hay[] = "Hello world!";
+	char needle[] = " wor";
+	char span[] = "Hell";
+	/*Tok test*/
+	char source[] = "Hello-Shalom-Ki ora";
+	char delin[] = "-";
 
+	/*Function test order*/
+	/*Length Test*/
+	LenTest(test_len);
+	/*Cmp and case compare tests*/
+	CmpTest(test_cmp, exp_cmp_equ);
+	CmpTest(test_cmp, exp_cmp_gtr);
+	CmpTest(test_cmp, exp_cmp_lss);
+	CmpTest(test_cmp, exp_cmp_lng);
+	CaseCmpTest(test_cmp, exp_cmp_lss);
+	/*Copy, copy n and Dup tests*/
+	CpyTest(test, exp, cpy);
+	CpynTest(n_test, n_exp, cpy, n);
+	CpynTest(n_test_lng, n_exp_lng, cpy, n);
+	DupTest(cpy);
+	/*Chr test*/
+	ChrTest(test, ch);
+	/*Cat and Cat n tets*/
+	CatTest(base, base, append);
+	CatnTest(base, base, append, n);
+	/*Str and Spn tests*/
+	StrTest(hay, needle);
+	SpnTest(hay, span);
+	/*Tok test*/
+	TokTest(source, delin);
+	/*Palindrome tests*/
+	PalindromeTest();
+	/*Swap test*/
+	SwapTest();
+
+	return 0;
 }
--- c/ws2/ws2.c
+++ c/ws2/ws2.c
@@ -6,58 +6,55 @@
 size_t Strlen(const char *string)
 {
 	int length = 0;
-	while (*(string + length) != 0x0)
+	while (*string != 0x0)
 	{
+		++string;
 		++length;
 	}
 
-	return (length+1);
+	return length;
 }
 
 int Strcmp(const char *str1, const char *str2)
 {
 	int len1 = Strlen(str1);
 	int len2 = Strlen(str2);
-	int tot1 = 0;
-	int tot2 = 0;
+	int compare = 0;
 	int i = 0;
 	int j = 0;
 	while((i <= len1) || (j <= len2))
 	{
 		if ((i >= len1) && (j <= len2))
 		{
-			tot2 += *(str2+j);
+			compare -= *(str2+j);
 		}else if ((i <= len1) && (j >= len2))
 		{
-			tot1 += *(str1+i);
-		}else
+			compare += *(str1+i);
+		}else 
 		{
-			tot1 += *(str1+i);
-			tot2 += *(str2+j);
+			compare = *(str1+i) - *(str2+j);
+		}
+		if (compare != 0)
+		{
+			return compare;
+			break;
 		}
 		++i;
 		++j;
 	}
-	/*if (tot1 > tot2)
-	{
-		return 1;
-	}else if (tot1 < tot2)
-	{
-		return -1;
-	}else if (tot1 == tot2)
-	{
-		return 0;
-	}*/
-	return (tot1 - tot2);
+	return 0;
 }
 
 char *Strcpy(char *str2, const char *str1)
 {
 	int length = Strlen(str1);
-	for (int i = 0; i < length; ++i)
+	for (int i = 0; i <= length; ++i)
 	{
-		*(str2+i) = *(str1+i);
+		*str2 = *str1;
+		++str1;
+		++str2;
 	}
+	return 0;
 }
 
 char *Strncpy(char *str2, const char *str1, size_t n)
@@ -65,27 +62,30 @@ char *Strncpy(char *str2, const char *str1, size_t n)
 
 	for (int i = 0; i < n; ++i)
 	{
-		*(str2+i) = *(str1+i);
+		*str2 = *str1;
+		++str1;
+		++str2;
 	}
+	return 0;
 }
 
 char *StrLowerConvert(char *str)
 {
-	char *output;
 	int length = Strlen(str);
 	for (int i = 0; i <= length; ++i)
 	{
-		if ((*(str+i) > 0x40) && (*(str+i) < 0x5b))
+		if ((*str > 0x40) && (*str < 0x5b))
 		{
-			*(str+i) = *(str+i)+0x20;
+			*str = *str+0x20;
 		}
+		++str;
 	}
-	
+	return 0;
 }
 
 int Strcasecmp(char *str1, char *str2)
 {
-	StrLowerConvert(str2);
+	StrLowerConvert(str1);
 	StrLowerConvert(str2);
 	int result = Strcmp(str1, str2);
 
@@ -97,12 +97,14 @@ char *Strchr(char *str, int ch)
 	int length = Strlen(str);
 	for (int i = 0; i <= length; ++i)
 	{
-		if (*(str+i) == ch)
+		if (*str == ch)
 		{
-			return (str+i);
+			return str;
 			break;
 		}
+		++str;
 	}
+	return 0;
 }
 
 char *Strdup(char *str)
@@ -114,44 +116,55 @@ char *Strdup(char *str)
 
 char Strcat(char *dest, const char *src)
 {
-	int length = Strlen(dest) -1;
+	int length = Strlen(dest);
 	int added = Strlen(src);
+	dest += length;
 	for (int i = 0; i < added; ++i)
 	{
-		*(dest+i+length) = *(src+i);	
+		*dest = *src;
+		++dest;
+		++src;	
 	}
+	return 0;
 }
 
 char Strncat(char *dest, const char *src, size_t n)
 {
-	int length = Strlen(dest) -1;
-	char output[length+n];
+	int length = Strlen(dest);
+	dest += length;
 	for (int i = 0; i < n; ++i)
 	{
-		*(dest+i+length) = *(src+i);	
+		*dest = *src;
+		++dest;
+		++src;	
 	}
+	return 0;
 }
 
 char *Strstr(char *hay, char *needle)
 {
-	int length = Strlen(hay);
-	char *output;	
+	//char *output;
+	int length = Strlen(hay);	
 	int j = 0;
 	for(int i = 0; i <= length; ++i)
 	{
-		if(*(hay+i) == *(needle+j))
+		if(*hay == *(needle+j))
 		{
+			//++needle;
 			++j;
 		}else if (*(needle+j) == 0x0)
 		{	
-			output = (hay+i-j);
-			return output;
+			//output = hay - j;
+			hay-=j;
+			return hay;
 			break;
 		}else
 		{
 			j = 0;
 		}
+		++hay;
 	}
+	return 0;
 }
 
 
@@ -160,24 +173,27 @@ size_t Strspn(const char *initial, const char *span)
 	int length_init = Strlen(initial)-1;
 	int length_span = Strlen(span);
 	int char_count = 0;
-	for(int i = 0; i < length_init; ++i)
+	for(int i = 0; i <= length_init; ++i)
 	{
-		for(int j = 0; j < length_span; ++j)
+		for(int j = 0; j <= length_span; ++j)
 		{
-			if(*(initial+i) == *(span+j))
+			if(*initial == *span)
 			{
 				++char_count;
 				break;
-			}else if((*(span+j) == 0x0) && (char_count != 0))
+			}else if((*span == 0x0) && (char_count != 0))
 			{
 				return char_count;
 			}
+			++span;
 		}
+		++initial;
 	}
 	if (char_count == 0)
 	{
 		return char_count;
 	}
+	return 0;
 }
 
 char *Strtok(char *str, const char *delin)
@@ -199,25 +215,28 @@ char *Strtok(char *str, const char *delin)
 		{
 			return 0x0;
 			break;
-		}else if(*(buffer+i) == *delin)
+		}else if(*buffer == *delin)
 		{
 			previous = ++i;
 			return output;
 			break;
-		}else if(*(buffer+i) == 0x0)
+		}else if(*buffer == 0x0)
 		{
 			end = 1;
 			return output;
 			break;
 		}
-		*(output+i-previous) = *(buffer+i);
+		*(output-previous) = *buffer;
+		++buffer;
+		++output;
 	}
+	return 0;
 }
 
 int IsPalindrome (char *str)
 {
 	int l = 0;
-	int r = Strlen(str) -2;
+	int r = Strlen(str) -1;
 	StrLowerConvert(str);
 	while (l <= r)
 	{
@@ -233,11 +252,12 @@ int IsPalindrome (char *str)
 	{
 		return 1;
 	}
+	return 0;
 }
 
 void Boom7(int from, int to)
 {
-	for(from; from <= to; ++from)
+	for( ; from <= to; ++from)
 	{
 		int hit = 0;
 		for(int i = from; i > 0; i /= 10)
@@ -288,7 +308,7 @@ void RmSpaces(char *str)
 		}
 		++j;
 	}
-	for(j; j <= length; ++j)
+	for( ; j <= length; ++j)
 	{
 		*(buffer+j) = 0x0;
 	}
