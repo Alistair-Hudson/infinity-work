45e52a3959cd073a83cbee884a49b3ca678b97e5
alistair.hudson <alistair.hudson@gmal.com>
2020-01-15 19:02:44 +0200

    started on ws3

---
+++ c/ws3/ws3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int Josephus(int regiment)
+{
+
+}
+
+void Datatypes()
+{
+	printf("char is size of %d", sizeof(int));
+}
+
+
+
+int main (argc, argv, envp)
+{
+	int regiment[] = {1, 2, 3, 4};
+	
+	Datatypes();
+
+	return 0;
+}
