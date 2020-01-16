8604552aefe18769c190cc7bf1b21587ea061631
alistair.hudson <alistair.hudson@gmal.com>
2020-01-16 19:55:41 +0200

    continued work on ws3, struggling with envp

--- c/ws3/ws3.c
+++ c/ws3/ws3.c
@@ -1,24 +1,133 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "ws3.h"
+#include <time.h>
 
-int Josephus(int regiment)
+unsigned int AliJosephus(unsigned int number_of_soldiers)
 {
-
+	int *regiment = malloc(number_of_soldiers);
+	unsigned int survivour = 0;
+	unsigned int i = 0;
+	int kill = 2;
+	int lucky;
+	do
+	{
+		regiment[i] = survivour;
+		if ( i == survivour)
+		{
+			survivour = 1;
+		}else
+		{
+			survivour += kill;
+		}
+		++i;
+	}while(i <= number_of_soldiers);
+	lucky = regiment[number_of_soldiers];
+	free(regiment);
+	return lucky;
 }
+
+unsigned int Josephus(unsigned int number_of_soldiers)
+{
+	int *regiment = malloc(number_of_soldiers-1);
+	unsigned int i = 0;
+	unsigned int next;
+	while(i < number_of_soldiers)
+	{
+		if(i == (number_of_soldiers-1))
+		{
+			regiment[i] = 0;
+		}else
+		{
+			regiment[i] = i+1;
+		}
+		++i;
+	}
+	i = 0;
+	do
+	{
+		next = regiment[i];
+		regiment[i] = regiment[next];
+		i = regiment[next];
+		if(i == next)
+		{
+			free(regiment);
+			return (next+1);
+		}
+
+	}while (1);
+	free(regiment);
+	return 0;
+}
+		
 
 void Datatypes()
 {
-	printf("char is size of %d", sizeof(int));
+	printf("char is size of %ld\n", sizeof(char));
+	printf("int is size of %ld\n", sizeof(int));
+	printf("float is size of %ld\n", sizeof(float));
+	printf("double is size of %ld\n", sizeof(double));
+	printf("void is size of non-existant\n");
+}
+
+char *Copy(char *dest, const char *src)
+{
+	/*char *copy = (char *)malloc(strlen(src)+1);
+	strcpy(dest, src);*/
+	dest = strdup(src);
+	while(*dest)
+	{	
+		*dest = (char)tolower(*dest);
+		/*++copy;*/
+		++dest;
+	}
+	/*free(copy);
+	copy = NULL;*/
+	return 0;
+}
+
+void Print (char *string)
+{
+	printf("%s \n", string);
+}
+
+void EnvExercise(const char **evp)
+{
+	char **env = (char **)evp;
+	char *buffer;
+	while(*env)
+	{
+		buffer = (char *)malloc(strlen(*env)+1);
+		Copy(buffer, *env);		
+		Print(buffer);		
+		++env;
+		free(buffer);
+		buffer = NULL;
+	}
+			
 }
 
 
 
-int main (argc, argv, envp)
-{
-	int regiment[] = {1, 2, 3, 4};
-	
-	Datatypes();
+int main (int argc, char **argv, char **envp)
+{		
+	/*int result;
+	double time_taken;
+	clock_t t; 
+    t = clock(); 
+ 	result =  Josephus(15);
+    t = clock() - t; 
+  	time_taken = ((double)t)/CLOCKS_PER_SEC;
+    printf("Josephus took %f seconds to execute \n", time_taken); 
 
+	t = clock(); 
+ 	result =  AliJosephus(15);
+    t = clock() - t; 
+  	time_taken = ((double)t)/CLOCKS_PER_SEC;
+    printf("AliJosephus took %f seconds to execute \n", time_taken);*/
+	Datatypes();
+	printf("Josephus for 100 = %d \n", Josephus(100));
+	EnvExercise((const char **)envp);
 	return 0;
 }
---
+++ c/ws3/ws3.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+/*Josephus solution*/
+unsigned int Josephus(unsigned int number_of_soldiers);
+unsigned int AliJosephus(unsigned int number_of_soldiers);
+/*Data types and their sizes*/
+void Datatypes();
+/*Enviroment varibles call*/
+void EnvExercise(const char **envp);
+/*Copy to lower case*/
+char *Copy(char *dest, const char *src);
+/*Print stuffz*/
+void Print (char *string);
+
+#endif
