e04818f8bff71a58cf93a0057a535cb34d81f01d
alistair.hudson <alistair.hudson@gmal.com>
2020-01-29 18:36:32 +0200

    fixed ws5

--- c/ws5/ws5.c
+++ c/ws5/ws5.c
@@ -15,20 +15,15 @@ typedef struct _str_hand
 	cmp_t compare;
 	act_t action;
 }str_handler_t;
-struct str_handler_t str_handler[NUM_FUNCS];
 
-enum statuses{EXIT, SUCCESS, OPEN, NO_STRING, NO_INPUT, NO_RENAME, NO_REMOVE};
+static struct _str_hand str_handler[NUM_FUNCS];
+enum statuses{SUCCESS, EXIT, OPEN, NO_STRING, NO_INPUT, NO_RENAME, NO_REMOVE};
 
 static const int max_iterations = NUM_FUNCS -1;
 
-static int Compare (char *cmp_str)
+static int CommandCmp(char *cmp_str)
 {
 	int i = max_iterations;
-	if(NULL == cmp_str)
-	{
-		perror("Error: ");
-		return NO_STRING;
-	}
 	while(i > 1)
 	{
 		if(str_handler[i].compare(str_handler[i].string, cmp_str) == 0)
@@ -37,13 +32,24 @@ static int Compare (char *cmp_str)
 		}
 		--i;
 	}
+	return 0;
+}
+static int Compare (char *cmp_str)
+{
+	int action_call = 0;
+	if(NULL == cmp_str)
+	{
+		perror("Error: ");
+		return NO_STRING;
+	}
+	action_call = CommandCmp(cmp_str);
 	if('<' == *cmp_str)
 	{
 		strcpy(str_handler[1].string, cmp_str+1);
-		return 1;
+		action_call = 1;
 	}
 	strcpy(str_handler[0].string, cmp_str);
-	return 0;
+	return action_call;
 }
 
 static int Exit(char *filename)
@@ -85,23 +91,18 @@ static int CountLines(char *filename)
 {
 	FILE *file = fopen(filename, "r");
 	int line_count = 0;
+	char line[STRING_LIMIT];
 	if(NULL == file)
 	{
 		perror("Error: ");
 		return OPEN;
 	}
-	while (1)
+	do
 	{
-		if(fgetc(file) == '\n')
-		{
-			++line_count;
-		}
-		if (feof(file))
-		{
-			printf("Line count = %d\n", line_count);
-			break;
-		}
-	}
+		fgets(line, STRING_LIMIT, file);
+		++line_count;
+	}while (!EOF);
+	printf("Line count = %d\n", line_count);
 	return SUCCESS;
 }
 
@@ -110,7 +111,7 @@ static int AppendToStart(char *filename)
 	FILE *dest_file;
 	FILE *src_file;
 	int ret;
-	int chr;
+	char line[STRING_LIMIT];
 	dest_file = fopen("temp.txt", "a+");
 	src_file = fopen(filename, "a+");
 	if(NULL == dest_file)
@@ -129,15 +130,11 @@ static int AppendToStart(char *filename)
 		return NO_STRING;
 	}
 	fputs(str_handler[1].string, dest_file);
-	while(1)
+	do
 	{
-		chr = fgetc(src_file);
-		if(feof(src_file))
-		{
-			break;
-		}
-		fputc(chr, dest_file);
-	}
+		fgets(line, STRING_LIMIT, src_file);
+		fputs(line, dest_file);
+	}while(!EOF);
 	fclose(dest_file);
 	fclose(src_file);
 	ret = rename("temp.txt", filename);
@@ -148,22 +145,34 @@ static int AppendToStart(char *filename)
 	}
 	return SUCCESS;
 }
+static int InitializeHandler()
+{
+	strcpy(str_handler[2].string, "-remove\n");
+	strcpy(str_handler[3].string, "-count\n");
+	strcpy(str_handler[4].string, "-exit\n");
 
-str_handler[NUM_FUNCS] = {
-						{NULL, NULL, Append}, /*default*/
-						{NULL, NULL, AppendToStart},
-						{"-remove\n", strcmp, Remove},
-						{"-count\n", strcmp, CountLines},
-						{"-exit\n", strcmp, Exit}
-						};
+	str_handler[0].compare = NULL;
+	str_handler[1].compare = NULL;
+	str_handler[2].compare = strcmp;
+	str_handler[3].compare = strcmp;
+	str_handler[4].compare = strcmp;
+
+	str_handler[0].action = Append;
+	str_handler[1].action = AppendToStart;
+	str_handler[2].action = Remove;
+	str_handler[3].action = CountLines;
+	str_handler[4].action = Exit;
+	
+	return SUCCESS;
+}
 
 int EditFile(char *filename)
 {
 	int call;
-	int status = SUCCESS;
+	int status = InitializeHandler();
 	char input[STRING_LIMIT];
 
-	while(status)
+	while(!status)
 	{
 		fgets(input, STRING_LIMIT, stdin);
 		if(NULL == input)
