dfe0d8bbc8d1e1ed49fa21c45c7e85df6b942ca5
Alistair <alistair.hudson64@gmail.com>
2020-02-01 13:09:35 +0200

    continued work on ws9

--- c/ws9/ws9.c
+++ c/ws9/ws9.c
@@ -1,5 +1,5 @@
 #include <stdio.h> /*standard inputs and out puts. ie printf*/
-#include <stdlib.h> /**/
+#include <stdlib.h> /*for malloc*/
 #include <string.h>/*for string functions such ie strlen*/
 
 #define WORD_SIZE (sizeof(size_t))
@@ -35,7 +35,7 @@ void *Memcpy(void *dest, const void *src, size_t size)
 		*(size_t*)dest = *(size_t*)src;
 		*(size_t*)&dest += WORD_SIZE;
 		*(size_t*)&src += WORD_SIZE;
-		bytes_used = WORD_SIZE;
+		bytes_used += WORD_SIZE;
 	}
 	while (size > bytes_used)
 	{
@@ -48,12 +48,48 @@ void *Memcpy(void *dest, const void *src, size_t size)
 
 void *Memmove(void *dest, const void *src, size_t size)
 {
+	unsigned int bytes_used = 0;
+	
+	while ((0 != (size_t)dest % (size_t)WORD_SIZE) && (size >= bytes_used))
+	{
 
+		++bytes_used;
+	}
 }
 
 void *Memset(void  *dest, int character, size_t size)
 {
+	unsigned int bytes_used = 0;
+	void *buffer = malloc(WORD_SIZE);
+	void *buffer_start = buffer;
+	int buffer_fill = WORD_SIZE;
 
+	while (0 <= buffer_fill)
+	{
+		*(char*)buffer = (char)character;
+		++*(size_t*)&buffer;
+		--buffer_fill;
+	}
+	while ((0 != (size_t)dest % (size_t)WORD_SIZE) && (size >= bytes_used))
+	{
+		*(char*)dest = (char)character;
+		++*(size_t*)&dest;
+		++bytes_used;
+	}
+	while (size >= bytes_used + WORD_SIZE)
+	{
+		*(size_t*)dest = *(size_t*)buffer_start;
+		*(size_t*)&dest += WORD_SIZE;
+		bytes_used += WORD_SIZE;
+	}
+	while (size > bytes_used)
+	{
+		*(char*)dest = (char)character;
+		++*(size_t*)&dest;
+		++bytes_used;
+	}
+	free(buffer);
+	buffer = NULL;
 }
 
 char *Itoa(int value, char *str, int base)
@@ -83,7 +119,6 @@ int Atoi(const char *str)
 	}
 	do
 	{
-		printf("%d\n", value);
 		if(('0' <= *str) && ('9' >= *str))
 		{
 			value += *str -'0';
@@ -95,7 +130,7 @@ int Atoi(const char *str)
 		value *= 10;
 		++str;
 	}while(0 != *str);
-	return (minus*value);
+	return (minus*value/10);
 }
 
 int ISLittleEndian()
@@ -108,7 +143,7 @@ int main()
 {
 	char dest[] = "123456789abcdefg";
 	char src[] = "abcdefghijklmnop";
-	int x = Atoi("-129");
-	printf("after: %d %s\n", x, src);
+	Memset(dest, 'H', 13);
+	printf("after: %s\n", dest);
 	return 0;
 }	
