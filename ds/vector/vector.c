dbdbea6ba0a440a88ed185780f75fe984c498644
alistair.hudson <alistair.hudson@gmal.com>
2020-02-10 19:04:30 +0200

    finished vector ws

--- ds/vector/vector.c
+++ ds/vector/vector.c
@@ -1,10 +1,10 @@
-#include <stdlib.h>/*malloc*/
-#include <assert.h>/*assert*/
+#include <stdlib.h>		/*malloc*/
+#include <assert.h>		/*assert*/
 #include "vector.h"
 
 /*****MACROS*****/
-#define ELEMENT_SIZE (sizeof(void*))
-
+#define ELEMENT_SIZE 	(sizeof(void*))
+#define PUSH_ADJST		((VectorSize(vector)+1)*2)
 /*****STRUCTURE TO HANDLE THE STACK******/
 struct vector
 {
@@ -13,31 +13,22 @@ struct vector
 	void *top;
 };
 
-/******FUNCTION DECLARATIONS********/
-vector_t *VectorCreate(size_t capacity);
-void VectorDestroy(vector_t *vector);
-
-void VectorPopBack(vector_t *vector);
-int VectorPushBack(vector_t *vector, void *data);
-
-size_t VectorSize(const vector_t *vector);
-int VectorIsEmpty(const vector_t *vector);
-size_t VectorCapacity(const vector_t *vector);
-
-void VectorSetElement(vector_t *vector, size_t pos, void *data);
-void *VectorGetElement(vector_t *vector, size_t pos);
-
-int VectorReserve(vector_t *vector, size_t new_capacity);
-int VectorShrinkToFit(vector_t *vector);
-
 /******FUNCTION DEFINITIONS********/
 vector_t *VectorCreate(size_t capacity)
 {
 	vector_t *_vector = malloc(sizeof(struct vector) * ELEMENT_SIZE);
-	assert(0 != _vector);
+	if(0 == _vector)
+	{
+		return 0;
+	}
 	_vector->base = malloc(capacity * ELEMENT_SIZE);
+	if(0 == _vector->base)
+	{
+		return 0;
+	}
 	_vector->max = (size_t *)_vector->base + capacity;
 	_vector->top = _vector->base;
+
 	return _vector;
 }
 
@@ -66,24 +57,27 @@ size_t VectorCapacity(const vector_t *vector)
 
 int VectorPushBack(vector_t *vector, void *data)
 {
+	int reserve_check = 0;
 	size_t next_top_loc = (size_t)vector->top + ELEMENT_SIZE;
+
 	assert(NULL != vector);
 	if ((size_t)vector->max <= next_top_loc)
 	{
-		return 0;
+		reserve_check = VectorReserve(vector, PUSH_ADJST);
+		if(reserve_check)
+		{
+			return 1;
+		}
 	}
 	*(size_t**)vector->top = data;
 	*(size_t*)&vector->top += ELEMENT_SIZE;
-	return 1;
+	return 0;
 }
 
-void *VectorGetValue(vector_t *vector, size_t pos)
+void *VectorGetElement(vector_t *vector, size_t pos)
 {
 	assert(NULL != vector);
-	if(VectorIsEmpty(vector))
-	{
-		return 0;
-	}
+
 	return (size_t*)((size_t)(vector->base)+(pos * ELEMENT_SIZE));
 }
 
@@ -104,4 +98,43 @@ void VectorSetElement(vector_t *vector, size_t pos, void *data)
 {
 	assert(NULL != vector);
 	*(size_t**)((size_t)vector->base + (pos * ELEMENT_SIZE)) = data;
+}
+
+int VectorShrinkToFit(vector_t *vector)
+{
+	size_t size = 0;
+	int reserve_check = 0;
+
+	assert(NULL != vector);
+	size = VectorSize(vector);
+
+	reserve_check = VectorReserve(vector, size);
+
+	if(reserve_check)
+	{
+		return 1;
+	}
+
+	return 0;
+}
+
+int VectorReserve(vector_t *vector, size_t new_capacity)
+{
+	size_t size = 0;
+	void *buffer = 0;
+	
+	assert(NULL != vector);
+	size = VectorSize(vector);	
+	buffer = realloc(vector->base, (new_capacity * ELEMENT_SIZE));
+	if(0 == buffer)
+	{
+		return 1;
+	}
+
+	vector->base = buffer;
+
+	vector->max = (size_t *)vector->base + new_capacity;
+	vector->top = (size_t *)vector->base + size;
+
+	return 0;
 }
--- ds/vector/vector_test.c
+++ ds/vector/vector_test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "vector.c"
 
-#define NEED_TO_PASS (7)
+#define NEED_TO_PASS (10)
 int main()
 {	
 	size_t pass = 0;
@@ -11,40 +11,66 @@ int main()
 	if(80 == VectorCapacity(ptr))
 	{
 		++pass;
+		printf("Cap\n");
 	}
 	if(VectorIsEmpty(ptr))
 	{
 		++pass;
+		printf("Emp\n");
 	}
 	if(0 == VectorSize(ptr))
 	{
 		++pass;
+		printf("Size\n");
 	}
+
 	VectorPushBack(ptr, (void*)88);
 	VectorPushBack(ptr, (void*)666);
 	VectorPushBack(ptr, (void*)13);
 	if(3 == VectorSize(ptr))
 	{
 		++pass;
+		printf("Push\n");
 	}
 	VectorPopBack(ptr);
 	if(2 == VectorSize(ptr))
 	{
 		++pass;
+		printf("Pop\n");
 	}
 	if(666 == *(size_t*)VectorGetValue(ptr, 1))
 	{
 		++pass;
+		printf("Get\n");
 	}
 	VectorSetElement(ptr, 1, (void*)101);
 	if(101 == *(size_t*)VectorGetValue(ptr, 1))
 	{
 		++pass;
+		printf("Set\n");
 	}
+	VectorShrinkToFit(ptr);
+	if(2 == VectorCapacity(ptr))
+	{
+		++pass;
+		printf("Shrink\n");
+	}
+	if(101 == *(size_t*)VectorGetValue(ptr, 1))
+	{
+		++pass;
+	}
+	VectorPushBack(ptr, (void*)666);
+	VectorPushBack(ptr, (void*)13);
+	if(6 == VectorCapacity(ptr))
+	{
+		++pass;
+		printf("Reserve\n");
+	}
+
 
 	if(NEED_TO_PASS == pass)
 	{
-		printf("all passed\n");
+		printf("All Passed\n");
 	}
 	VectorDestroy(ptr);
 	
