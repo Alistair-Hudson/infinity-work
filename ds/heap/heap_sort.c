20df8b2e68093d631b9fd75ccca8b2ccdf22afad
Alistair <alistair.hudson64@gmail.com>
2020-04-20 09:49:52 +0300

    heap sort pseudo code completed

--- ds/heap/heap_sort.c
+++ ds/heap/heap_sort.c
@@ -18,4 +18,35 @@
 /******TYPEDEFS, GLOBAL VARIABLES AND INTERNAL FUNCTIONS******/
 
 /******FUNCTIONS******/
+int HeapSort(heap_t* heap)
+{
+	/*assert heap is not NULL*/
 
+	/*perform a quick sort (merge sort) on the heap with a parameter*/
+}
+
+static void HeapQSort(heap_t* heap)
+{
+	/*if start of vector location equals the end*/
+		/*return*/
+
+	/*if the the start elementis is less than the end element*/
+		/*create a partition index*/
+		
+		/*perform recursion on lower half*/
+		/*perform recursion on upper half*/
+}
+
+static char* HeapQSPartion*()
+{
+	
+	/*from the begining to the last element*/
+		/*if element is before pivot point*/
+			/*swap*/
+			/*move to next element*/
+}
+
+static HeapQSSwap()
+{
+	/*perform a swap*/
+}
