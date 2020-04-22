c578fe35bf3d03e4add60ce000359588b56e93ee
Alistair <alistair.hudson64@gmail.com>
2020-04-22 17:54:40 +0300

    started signal ping-pong

---
+++ system_programming/signals_ping-pong.c
@@ -0,0 +1,35 @@
+/******************************************************************************
+ *	Title:		Signal Ping-Pong
+ *	Authour:	Alistair Hudson
+ *	Reviewer:	
+ *	Version:	22.04.2020.0
+ ******************************************************************************/
+#include <stdlib.h>		/* malloc, free */
+#include <assert.h>		/* assert */
+#include <string.h>		/* memcpy */
+#include <stdio.h>		/* printf */
+#include <sys/types.h>	/* fork */
+#include <unistd.h>		/* fork */
+#include <sys/wait.h>	/* wait */
+#include <errno.h>		/* perror */
+
+/******MACROS******/
+#define ASSERT_NOT_NULL(ptr)	(assert(NULL != ptr))
+
+/******TYPEDEFS, GLOBAL VARIABLES AND INTERNAL FUNCTIONS******/
+
+/******FUNCTIONS******/
+static void Ping()
+{
+
+}
+
+static void Pong()
+{
+
+}
+
+int main ()
+{
+	return 0;
+}
