6b828f9f29a7d6d182fe2dc5d7f4ea75797b8eb7
Alistair <alistair.hudson64@gmail.com>
2020-04-23 08:39:19 +0300

    continued work on ping pong

--- system_programming/ping_pong/ping_pong.c
+++ system_programming/ping_pong/ping_pong.c
@@ -12,24 +12,62 @@
 #include <unistd.h>		/* fork */
 #include <sys/wait.h>	/* wait */
 #include <errno.h>		/* perror */
+#include <signal.h>		/* signal */
 
 /******MACROS******/
 #define ASSERT_NOT_NULL(ptr)	(assert(NULL != ptr))
 
 /******TYPEDEFS, GLOBAL VARIABLES AND INTERNAL FUNCTIONS******/
+static void Ping(int);
+
+static void Pong(int);
 
 /******FUNCTIONS******/
-static void Ping()
+static void Ping(int signum)
 {
-
+	signal(SIGUSR1, Ping);
+	printf("|o     |\n\n");
 }
 
-static void Pong()
+static void Pong(int signum)
 {
+	signal(SIGUSR2, Pong);
+	printf("|     o|\n\n");
 
 }
 
 int main ()
 {
+	pid_t pid = 0;
+
+	pid = fork();
+	
+	if (0 > pid)
+	{
+		perror("fork");
+		exit(1);
+	}
+
+	if (0 == pid)
+	{
+		signal(SIGUSR1, Ping);
+		while(1)
+		{
+
+/*			sleep(5);
+			kill(getppid, SIGUSR2);
+*/		}
+	}
+	else
+	{
+		kill(pid, SIGUSR1);
+/*		signal(SIGUSR2, Pong);
+		while(1)
+		{
+			sleep(5);
+			kill(pid, SIGUSR1);
+		}*/
+	}
+
 	return 0;
 }
