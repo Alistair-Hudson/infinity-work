edcd4f1caa26fc07ab32da4b85f129ae944f055c
alistair.hudson <alistair.hudson64@gmail.com>
2020-04-23 17:55:17 +0300

    added files for ping pong ex2 and 3

--- system_programming/ping_pong/ping2.c
+++ system_programming/ping_pong/ping2.c
@@ -1,5 +1,5 @@
 /******************************************************************************
- *	Title:		Signal Ping-Pong
+ *	Title:		Signal Ping
  *	Authour:	Alistair Hudson
  *	Reviewer:	
  *	Version:	22.04.2020.0
@@ -27,38 +27,21 @@ static int hit_back = 0;
 
 void hit(int);
 
-int PingPong1(void);
+int PingPong2(void);
 
 /******FUNCTIONS******/
 int main()
 {
 	int status = 0;
 
-	status = PingPong1();
+	status = PingPong2();
 
 	return status;
 }
 
-void Hit(int signum)
-{
-	hit_back = 1;
-}
-
-int PingPong1(void)
+int PingPong2(void)
 {
 	pid_t pid = 0;
-	action_handler_t parent = {0};
-	action_handler_t child = {0};
-	int signum = 0;
-	pid_t id = 0;
-	char str[10] = {0};
-	size_t rally = 0;
-
-	parent.sa_handler = Hit;
-	child.sa_handler = Hit;
-
-	sigaction(SIGUSR1, &parent, NULL);
-	sigaction(SIGUSR2, &child, NULL);
 
 	pid = fork();
 	
@@ -67,43 +50,23 @@ int PingPong1(void)
 		perror("Fork error");
 		return 1;
 	}
-
+	
 	if (0 == pid)
 	{
-		signum = SIGUSR1;
-		hit_back = 1;
-		id = getppid();
-		strcpy(str, "|o    |");
+		char* args[] = {"./", NULL};
+
+		execv(args[0], args);
 	}
 	else
 	{
-		signum = SIGUSR2;
-		id = pid;
-		strcpy(str, "|    o|");
+		
 	}
 
-	while(1)
-	{
-		if(1 == hit_back)
-		{
-			printf("%s\n", str);
-			sleep(1);
-			kill(id, signum);
-			hit_back = 0;
-			if (0 < pid)
-			{
-				++rally;
-			}
-			if (10 <= rally)
-			{
-				kill(id, SIGQUIT);
-				return 0;				
-			}
-		}
-	}
-
-	return 1;
+	return 0;
 }
 
-
+void Hit(int signum)
+{
+	hit_back = 1;
+}
 
---
+++ system_programming/ping_pong/ping3.c
@@ -0,0 +1,30 @@
+/******************************************************************************
+ *	Title:		Signal Ping ex3
+ *	Authour:	Alistair Hudson
+ *	Reviewer:	
+ *	Version:	22.04.2020.0
+ ******************************************************************************/
+#define _USE_POSIX199309
+#define _XOPEN_SOURCE
+
+#include <stdlib.h>		/*  */
+#include <assert.h>		/* assert */
+#include <string.h>		/*  */
+#include <stdio.h>		/* printf */
+#include <sys/types.h>	/* fork */
+#include <unistd.h>		/* fork */
+#include <sys/wait.h>	/* wait */
+#include <errno.h>		/* perror */
+#include <signal.h>		/* signal */
+
+/******MACROS******/
+#define ASSERT_NOT_NULL(ptr)	(assert(NULL != ptr))
+
+/******TYPEDEFS, GLOBAL VARIABLES AND INTERNAL FUNCTIONS******/
+typedef struct sigaction action_handler_t;
+
+static int hit_back = 0;
+
+void hit(int);
+
+/******FUNCTIONS******/
