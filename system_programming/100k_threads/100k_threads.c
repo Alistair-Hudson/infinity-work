dc58681f65fec8d8a50123518a88bca2f0f61220
alistair.hudson <alistair.hudson64@gmail.com>
2020-04-30 11:17:21 +0300

    finished detaching threads

--- system_programming/100k_threads/100k_threads.c
+++ system_programming/100k_threads/100k_threads.c
@@ -10,11 +10,12 @@
 #include <stdio.h>		/* printf */
 #include <errno.h>		/* perror */
 #include <pthread.h>	/* pthread functions */
+#include <time.h>		/* time */
 
 /******MACROS******/
 #define ASSERT_NOT_NULL(ptr)	(assert(NULL != (ptr)))
 #define MAX_THREADS				(100000)
-
+#define MAX_ERROR_LOOP			(100)
 /******TYPEDEFS, GLOBAL VARIABLES AND INTERNAL FUNCTIONS******/
 static size_t array[MAX_THREADS] = {0};
 static size_t thread = 0;
@@ -23,8 +24,6 @@ static size_t thread = 0;
 void* SetIndex(void* arg)
 {
 	(void)arg;
-	++thread;
-	array[thread-1] = thread-1; 
 	return NULL;	
 }
 
@@ -33,15 +32,28 @@ int main()
 	/*Initialise threads*/
 	pthread_t threads[MAX_THREADS];
 	size_t i = 0;
+	time_t start = time(NULL);
 
 	/*for from 0 to the Max number of threads*/
 	for (i = 0; i < MAX_THREADS; ++i)
 	{
+		size_t error_count = 0;
+		array[i] = i;
 		/*set each thread to the value of its index*/
-		pthread_create(&threads[i], NULL, SetIndex, (void*)NULL );
+		while(pthread_create(&threads[i], NULL, SetIndex, &array[i]))
+		{
+			/*++error_count;
+			if (MAX_ERROR_LOOP == error_count)
+			{
+				perror("Thread create failed");
+				printf("Thread break at %ld\n", i);
+				return 1;
+			}*/
+		}
+		pthread_detach(threads[i]);
 	}
 	/*Check time performance*/
-
+	printf("Time take = %ld\n", start - time(NULL));
 	/*sleep for 10 seconds*/
 	sleep(10);
 
@@ -50,7 +62,7 @@ int main()
 	{
 		if (i != array[i])
 		{
-			printf("failed at index %d = %d\n", i, array[i]);
+			printf("failed at index %ld = %ld\n", i, array[i]);
 			return 1;
 		}
 	}
