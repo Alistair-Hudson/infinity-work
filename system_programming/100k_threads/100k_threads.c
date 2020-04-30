d935f8c2e5e268a880f2c613bc6ff401e8e52e97
alistair.hudson <alistair.hudson64@gmail.com>
2020-04-30 14:05:45 +0300

    working on threads ex4

--- system_programming/100k_threads/100k_threads.c
+++ system_programming/100k_threads/100k_threads.c
@@ -14,11 +14,14 @@
 
 /******MACROS******/
 #define ASSERT_NOT_NULL(ptr)	(assert(NULL != (ptr)))
-#define MAX_THREADS				(100000)
-#define MAX_ERROR_LOOP			(100)
+#define MAX_THREADS				(NUMBER)
+#define THREAD_LIMIT			(32000)
+#define NUMBER					(1234567890)
+#define MAX_RUNS				(MAX_THREADS / THREAD_LIMIT)
+
 /******TYPEDEFS, GLOBAL VARIABLES AND INTERNAL FUNCTIONS******/
-static size_t array[MAX_THREADS] = {0};
-static size_t thread = 0;
+static size_t array[THREAD_LIMIT] = {0};
+static double sum_of_divisors = 0;
 
 /******FUNCTIONS******/
 void* SetIndex(void* arg)
@@ -27,33 +30,69 @@ void* SetIndex(void* arg)
 	return NULL;	
 }
 
+void* SumOfDivisors(void* arg)
+{
+	int sum_of_divisors = 0;
+	size_t i = 0;
+	size_t num = (*(size_t*)arg +1) * 100;
+	
+	for (i = (*(size_t*)arg * 100); i < num; ++i)
+	{
+		if (0 == (NUMBER % i))
+		{
+			sum_of_divisors += i;
+		}
+	}
+	return NULL;
+}
+
 int main()
 {
 	/*Initialise threads*/
-	pthread_t threads[MAX_THREADS];
+	pthread_t threads[THREAD_LIMIT];
 	size_t i = 0;
+	size_t j = 0;
 	time_t start = time(NULL);
-
-	/*for from 0 to the Max number of threads*/
-	for (i = 0; i < MAX_THREADS; ++i)
+	for (j = 0; j <= MAX_RUNS; ++j)
 	{
-		size_t error_count = 0;
-		array[i] = i;
-		/*set each thread to the value of its index*/
-		while(pthread_create(&threads[i], NULL, SetIndex, &array[i]))
+		size_t offset = THREAD_LIMIT * j;
+		/*for from 0 to the Max number of threads*/
+		for (i = 0; i < THREAD_LIMIT; ++i)
 		{
-			/*++error_count;
-			if (MAX_ERROR_LOOP == error_count)
+			if (MAX_THREADS <= (i+offset))
 			{
-				perror("Thread create failed");
-				printf("Thread break at %ld\n", i);
+				break;
+			}
+			array[i] = i + offset;
+			/*set each thread to the value of its index*/
+			while(pthread_create(&threads[i], NULL, SumOfDivisors, &array[i]))
+			{
+			}
+	/*This is for Ex3
+			if(pthread_detach(threads[i]))
+			{
+				perror("Thread detach failed");
 				return 1;
-			}*/
+			}
+	*/	}
+
+		/*Join threads*/
+		for (i = 0; i < THREAD_LIMIT; ++i)
+		{
+			if (MAX_THREADS <= (i+offset))
+			{
+				break;
+			}
+			if (pthread_join(threads[i], NULL))
+			{
+				perror("Thread join failed");
+				printf("run = %ld", j);
+				return 1;
+			}
 		}
-		pthread_detach(threads[i]);
 	}
 	/*Check time performance*/
-	printf("Time take = %ld\n", start - time(NULL));
+	printf("Time taken = %ld\n", time(NULL) - start);
 	/*sleep for 10 seconds*/
 	sleep(10);
 
@@ -67,5 +106,6 @@ int main()
 		}
 	}
 	printf("Success\n");
+	printf("for the number %f the sum of all divisors = %f\n", NUMBER, sum_of_divisors);
 	return 0;
 }
