c5aa13486ce4012bb1100e40f4631b99f0d7a151
alistair.hudson <alistair.hudson64@gmail.com>
2020-05-07 10:49:08 +0300

    Working on reading and writeing for producr consumer

--- system_programming/producer_consumer/producer_consumer.c
+++ system_programming/producer_consumer/producer_consumer.c
@@ -2,7 +2,7 @@
  *	Title:		Producer Consumer
  *	Authour:	Alistair Hudson
  *	Reviewer:	
- *	Version:	04.05.2020.0
+ *	Version:	07.05.2020.1
  ******************************************************************************/
 #include <stdlib.h>		/* atoi */
 #include <assert.h>		/* assert */
@@ -15,6 +15,9 @@
 #include <string.h>		/* strstr */
 #include <ctype.h>		/* isdigit */
 #include <semaphore.h>	/* semaphore functions */
+
+#include "slist.h"		/* slist functions */
+#include "queue.h"		/* queue functions */
 
 /******MACROS******/
 #define ASSERT_NOT_NULL(ptr)	(assert(NULL != (ptr)))
@@ -32,35 +35,33 @@ typedef struct EX1
 
 typedef struct EX21
 {
-	int is_empty;
+	int list_size;
 	pthread_mutex_t in_use_mutex;
-	int buffer;
+	list_t* buffer;
 }EX21_t;
 
 typedef struct EX22
 {
 	sem_t is_empty;
 	pthread_mutex_t in_use_mutex;
-	int buffer;
+	list_t* buffer;
 }EX22_t;
 
 typedef struct EX31
 {
 	sem_t free_space;
-	sem_t read;
+	sem_t size;
 	pthread_mutex_t in_use_mutex;
-	int que_size;
-	int queue[QUE_SIZE];
+	queue_t* queue;
 }EX31_t;
 
 typedef struct EX32
 {
-	sem_t written;
-	sem_t read;
+	sem_t free_space;
+	sem_t size;
 	pthread_mutex_t write_in_use;
 	pthread_mutex_t read_in_use;
-	int que_size;
-	int queue[QUE_SIZE];
+	queue_t* queue;
 }EX32_t;
 
 typedef struct EX41
@@ -68,7 +69,7 @@ typedef struct EX41
 	int read;
 	int write;
 	pthread_mutex_t in_use_mutex;
-	int queue[QUE_SIZE];
+	int queue;
 }EX41_t;
 
 typedef struct EX42
@@ -127,7 +128,7 @@ int main()
 {
 	int status = 0;
 
-	status = SingleProdCon();
+	status = FSQProdCon1();
 
 	return status;
 }
@@ -194,7 +195,13 @@ int MultiProdCon1()
 		perror("Mutex init failed");
 		return 1;
 	}
-	
+	buffer.buffer = SListCreate();
+	if (NULL == buffer.buffer)
+	{
+		printf("List creation failed\n");
+		return 1;
+	}	
+
 	pthread_create(&tid[0], NULL, Producer21, &buffer);
 	pthread_create(&tid[1], NULL, Consumer21, &buffer);
 
@@ -206,6 +213,8 @@ int MultiProdCon1()
 	pthread_join(tid[1], NULL);
 	pthread_join(tid[2], NULL);
 	pthread_join(tid[3], NULL);
+	
+	SListDestroy(buffer.buffer);
 
 	return 0;
 }
@@ -213,15 +222,16 @@ int MultiProdCon1()
 void* Producer21(void* arg)
 {
 	EX21_t* buffer = arg;
+	char message[] =  "This is a message\n";
 	/*check if lock is in use*/
 		/*set lock to in use*/
 	pthread_mutex_lock(&buffer->in_use_mutex);
 
 		/*write to buffer*/
-	printf("Writer\n");
-		
+	printf("Sending message...\n");
+	SListInsertBefore(buffer->buffer, SListEnd(buffer->buffer), &message);	
 		/*set not empty*/
-	buffer->is_empty += 1;
+	buffer->list_size += 1;
 		/*free lock*/
 	pthread_mutex_unlock(&buffer->in_use_mutex);
 
@@ -231,20 +241,22 @@ void* Producer21(void* arg)
 void* Consumer21(void* arg)
 {
 	EX21_t* buffer = arg;
+
 	/*loop if lock is not empty*/
-	while (0 != buffer->is_empty)
+	while (0 != buffer->list_size)
 	{
 		/*check if lock is in use*/
 			/*set lock to in use*/
 		pthread_mutex_lock(&buffer->in_use_mutex);
 
 			/*read from buffer*/
-		printf("Reader\n");
+		printf("Message: %s\n", (char*)SListGetData(SListBegin(buffer->buffer)));
+		SListRemove(SListBegin(buffer->buffer));
 			/*decrease read_or_write*/
-		buffer->is_empty -= 1;
-		if (0 > buffer->is_empty)
+		buffer->list_size -= 1;
+		if (0 > buffer->list_size)
 		{
-			buffer->is_empty = 0;
+			buffer->list_size = 0;
 		}
 			/*free lock*/
 		pthread_mutex_unlock(&buffer->in_use_mutex);
@@ -264,6 +276,12 @@ int MultiProdCon2()
 		perror("Mutex init failed");
 		return 1;
 	}
+	buffer.buffer = SListCreate();
+	if (NULL == buffer.buffer)
+	{
+		printf("List creation failed\n");
+		return 1;
+	}	
 
 	pthread_create(&tid[0], NULL, Producer22, &buffer);
 	pthread_create(&tid[1], NULL, Consumer22, &buffer);
@@ -277,18 +295,23 @@ int MultiProdCon2()
 	pthread_join(tid[2], NULL);
 	pthread_join(tid[3], NULL);
 
+	SListDestroy(buffer.buffer);
+
 	return 0;
 }
 
 void* Producer22(void* arg)
 {
 	EX22_t* buffer = arg;
+	char message[] =  "This is a message\n";
+
 	/*check if lock is in use*/
 		/*set lock to in use*/
 	pthread_mutex_lock(&buffer->in_use_mutex);
 
 		/*write to buffer*/
-	printf("Writer\n");
+	printf("Sending message...\n");
+	SListInsertBefore(buffer->buffer, SListEnd(buffer->buffer), &message);	
 		/*increase semaphore*/
 	sem_post(&buffer->is_empty);
 		/*free lock*/
@@ -306,7 +329,8 @@ void* Consumer22(void* arg)
 				/*set lock to in use*/
 	pthread_mutex_lock(&buffer->in_use_mutex);
 				/*read from buffer*/
-	printf("Reader\n");
+	printf("Message: %s\n", (char*)SListGetData(SListBegin(buffer->buffer)));
+	SListRemove(SListBegin(buffer->buffer));
 				/*free lock*/
 	pthread_mutex_unlock(&buffer->in_use_mutex);
 
@@ -318,15 +342,20 @@ void* Consumer22(void* arg)
 int FSQProdCon1()
 {
 	pthread_t tid[20];
-	EX31_t buffer = {0, 0, 0, 0, {0}};
+	EX31_t buffer = {0, 0, 0, {0}};
 	int i = 0;
 	
-	buffer.que_size = QUE_SIZE;
 	if (pthread_mutex_init(&buffer.in_use_mutex, NULL))
 	{
 		perror("Mutex init failed");
 		return 1;
 	}
+	buffer.queue = QCreate();
+	if (NULL == buffer.queue)
+	{
+		printf("Queue creation failed\n");
+		return 1;
+	}	
 
 	sem_init(&buffer.free_space, 0, QUE_SIZE);
 
@@ -340,23 +369,25 @@ int FSQProdCon1()
 		pthread_join(tid[i], NULL);
 	}
 
+	QDestroy(buffer.queue);
+
 	return 0;
 }
 
 void* Producer31(void* arg)
 {
 	EX31_t* buffer = arg;
-	int write_index = 0;
+	char message[] = "This is a message\n";
+
 	/*check if not in_use*/
 		/*set in_use*/
 	sem_wait(&buffer->free_space);
 	pthread_mutex_lock(&buffer->in_use_mutex);
 		/*write to queue*/
-	printf("Writer\n");
-	sem_getvalue(&buffer->free_space, write_index);
-	write_index = buffer->que_size - write_index;
+	printf("Sending message...\n");
+	QEnqueue(buffer->queue, &message);
 
-	sem_post(&buffer->read);
+	sem_post(&buffer->size);
 		/*free lock*/
 	pthread_mutex_unlock(&buffer->in_use_mutex);
 
@@ -368,12 +399,13 @@ void* Consumer31(void* arg)
 	EX31_t* buffer = arg;
 
 	/*loop if read is equal to written*/
-	sem_wait(&buffer->read);
+	sem_wait(&buffer->size);
 	/*check if not in_use*/
 		/*set in_use*/
 	pthread_mutex_lock(&buffer->in_use_mutex);
 		/*read from buffer*/
-	printf("Reader\n");
+	printf("Message: %s\n", QPeek(buffer->queue));
+	QDequeue(buffer->queue);
 
 	sem_post(&buffer->free_space);
 		/*free lock*/
@@ -386,10 +418,9 @@ void* Consumer31(void* arg)
 int FSQProdCon2()
 {
 	pthread_t tid[20];
-	EX32_t buffer = {0, 0, 0, 0, 0, {0}};
+	EX32_t buffer = {0, 0, 0, 0, {0}};
 	int i = 0;
 
-	buffer.que_size = QUE_SIZE;
 	if (pthread_mutex_init(&buffer.write_in_use, NULL))
 	{
 		perror("Mutex init failed");
@@ -400,8 +431,14 @@ int FSQProdCon2()
 		perror("Mutex init failed");
 		return 1;
 	}
+	buffer.queue = QCreate();
+	if (NULL == buffer.queue)
+	{
+		printf("Queue creation failed\n");
+		return 1;
+	}
 
-	sem_init(&buffer.written, 0, QUE_SIZE);
+	sem_init(&buffer.free_space, 0, QUE_SIZE);
 
 	for (i = 0; i < 10; ++i)
 	{
@@ -413,24 +450,25 @@ int FSQProdCon2()
 		pthread_join(tid[i], NULL);
 	}
 
+	QDestroy(buffer.queue);
+
 	return 0;
 }
 
 void* Producer32(void* arg)
 {
 	EX32_t* buffer = arg;
-	int write_index = 0;
+	char message[] = "This is a message\n";
 
 	/*check if not in_use*/
 		/*set in_use*/
 	sem_wait(&buffer->free_space);
 	pthread_mutex_lock(&buffer->write_in_use);
 		/*write to queue*/
-	printf("Writer\n");
-	sem_getvalue(&buffer->free_space, write_index);
-	write_index = buffer->que_size - write_index;
+	printf("Sending message...\n");
+	QEnqueue(buffer->queue, &message);
 
-	sem_post(&buffer->read);
+	sem_post(&buffer->size);
 		/*free lock*/
 	pthread_mutex_unlock(&buffer->write_in_use);
 
@@ -442,14 +480,15 @@ void* Consumer32(void* arg)
 	EX32_t* buffer = arg;
 
 	/*loop if read is equal to written*/
-	sem_wait(&buffer->read);
+	sem_wait(&buffer->size);
 	/*check if not in_use*/
 		/*set in_use*/
 	pthread_mutex_lock(&buffer->read_in_use);
 		/*read from buffer*/
-	printf("Reader\n");
+	printf("Message: %s\n", QPeek(buffer->queue));
+	QDequeue(buffer->queue);
 
-	sem_post(&buffer->written);
+	sem_post(&buffer->free_space);
 		/*free lock*/
 	pthread_mutex_unlock(&buffer->read_in_use);
 
