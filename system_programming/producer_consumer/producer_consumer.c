29496cd820d03ce18677ec48ff6a63d0fda8524f
alistair.hudson <alistair.hudson64@gmail.com>
2020-05-07 08:54:56 +0300

    proof of concept for producer consumer complete, now working on reading and writing to buffer

--- system_programming/producer_consumer/producer_consumer.c
+++ system_programming/producer_consumer/producer_consumer.c
@@ -27,7 +27,7 @@
 typedef struct EX1
 {
 	int read_or_write;
-	char* buffer[BUFFER_LIMIT];
+	char buffer[BUFFER_LIMIT];
 }EX1_t;
 
 typedef struct EX21
@@ -46,9 +46,10 @@ typedef struct EX22
 
 typedef struct EX31
 {
-	sem_t written;
+	sem_t free_space;
 	sem_t read;
 	pthread_mutex_t in_use_mutex;
+	int que_size;
 	int queue[QUE_SIZE];
 }EX31_t;
 
@@ -58,16 +59,36 @@ typedef struct EX32
 	sem_t read;
 	pthread_mutex_t write_in_use;
 	pthread_mutex_t read_in_use;
+	int que_size;
 	int queue[QUE_SIZE];
 }EX32_t;
 
 typedef struct EX41
 {
 	int read;
-	int Write;
-	int in_use_mutex;
-	int *buffer;
+	int write;
+	pthread_mutex_t in_use_mutex;
+	int queue[QUE_SIZE];
 }EX41_t;
+
+typedef struct EX42
+{
+	int write_index;
+	int read_index;
+	pthread_mutex_t write_in_use;
+	pthread_mutex_t read_in_use;
+	int queue[QUE_SIZE];
+}EX42_t;
+
+typedef struct EX5
+{
+	int total_consumers;
+	int consumers_needed_to_read;
+	pthread_mutex_t in_use_mutex;
+	pthread_cond_t barrier;
+	int have_read_array;
+	int buffer;
+}EX5_t;
 
 int SingleProdCon(void);
 void* Producer1(void* buffer);
@@ -93,11 +114,26 @@ int OverUnderFlow1(void);
 void* Producer41(void* buffer);
 void* Consumer41(void* buffer);
 
-/******FUNCTIONS******/
+int OverUnderFlow2(void);
+void* Producer42(void* buffer);
+void* Consumer42(void* buffer);
 
+int Barrier(void);
+void* Producer5(void* buffer);
+void* Consumer5(void* buffer);
+
+/******FUNCTIONS******/
+int main()
+{
+	int status = 0;
+
+	status = SingleProdCon();
+
+	return status;
+}
 
 /*==ONE PRODUCER, ONE CONSUMER==*/
-int SingleProddCon()
+int SingleProdCon()
 {
 	pthread_t tid[2];
 	EX1_t buffer = {0, {0}};
@@ -114,14 +150,15 @@ int SingleProddCon()
 void* Producer1(void* arg)
 {
 	EX1_t* buffer = arg; 
+	char* message = "This is a mesage\n";
 	/*loop if buffer is not write*/
-	while (WRITE != __sync_lock_test_and_set(&buffer->read_or_write, WRITE))
+	while (0 != __sync_lock_test_and_set(&buffer->read_or_write, 1))
 	{
 		sleep(0);
 	}
 	/*copy message to buffer*/
 	printf("Sending Message...\n");
-	strncpy(*buffer->buffer, "This is a mesage\n", BUFFER_LIMIT);
+	strncpy(buffer->buffer, message, BUFFER_LIMIT);
 	/*releaase buffer*/
 	__sync_lock_release(&buffer->read_or_write);
 
@@ -132,12 +169,12 @@ void* Consumer1(void* arg)
 {
 	EX1_t* buffer = arg;
 	/*loop if buffer is not read*/
-	while (READ != __sync_lock_test_and_set(&buffer->read_or_write, READ))
+	while (0 != __sync_lock_test_and_set(&buffer->read_or_write, 1))
 	{
 		sleep(0);
 	}
 	/*copy message from buffer*/
-	printf("Message: %s\n", *buffer->buffer);
+	printf("Message: %s\n", buffer->buffer);
 	/*release buffer*/
 	__sync_lock_release(&buffer->read_or_write);
 
@@ -147,7 +184,6 @@ void* Consumer1(void* arg)
 
 /*==MULTIPLE PRODUCERS, MULTIPLE CONSUMERS==*/
 /*1*/
-
 int MultiProdCon1()
 {
 	pthread_t tid[4];
@@ -282,16 +318,17 @@ void* Consumer22(void* arg)
 int FSQProdCon1()
 {
 	pthread_t tid[20];
-	EX31_t buffer = {0, 0, 0, {0}};
+	EX31_t buffer = {0, 0, 0, 0, {0}};
 	int i = 0;
-
+	
+	buffer.que_size = QUE_SIZE;
 	if (pthread_mutex_init(&buffer.in_use_mutex, NULL))
 	{
 		perror("Mutex init failed");
 		return 1;
 	}
 
-	sem_init(&buffer.written, 0, QUE_SIZE);
+	sem_init(&buffer.free_space, 0, QUE_SIZE);
 
 	for (i = 0; i < 10; ++i)
 	{
@@ -309,13 +346,16 @@ int FSQProdCon1()
 void* Producer31(void* arg)
 {
 	EX31_t* buffer = arg;
-
+	int write_index = 0;
 	/*check if not in_use*/
 		/*set in_use*/
-	sem_wait(&buffer->written);
+	sem_wait(&buffer->free_space);
 	pthread_mutex_lock(&buffer->in_use_mutex);
 		/*write to queue*/
 	printf("Writer\n");
+	sem_getvalue(&buffer->free_space, write_index);
+	write_index = buffer->que_size - write_index;
+
 	sem_post(&buffer->read);
 		/*free lock*/
 	pthread_mutex_unlock(&buffer->in_use_mutex);
@@ -335,7 +375,7 @@ void* Consumer31(void* arg)
 		/*read from buffer*/
 	printf("Reader\n");
 
-	sem_post(&buffer->written);
+	sem_post(&buffer->free_space);
 		/*free lock*/
 	pthread_mutex_unlock(&buffer->in_use_mutex);
 
@@ -346,9 +386,10 @@ void* Consumer31(void* arg)
 int FSQProdCon2()
 {
 	pthread_t tid[20];
-	EX32_t buffer = {0, 0, 0, 0, {0}};
+	EX32_t buffer = {0, 0, 0, 0, 0, {0}};
 	int i = 0;
 
+	buffer.que_size = QUE_SIZE;
 	if (pthread_mutex_init(&buffer.write_in_use, NULL))
 	{
 		perror("Mutex init failed");
@@ -378,13 +419,17 @@ int FSQProdCon2()
 void* Producer32(void* arg)
 {
 	EX32_t* buffer = arg;
+	int write_index = 0;
 
 	/*check if not in_use*/
 		/*set in_use*/
-	sem_wait(&buffer->written);
+	sem_wait(&buffer->free_space);
 	pthread_mutex_lock(&buffer->write_in_use);
 		/*write to queue*/
 	printf("Writer\n");
+	sem_getvalue(&buffer->free_space, write_index);
+	write_index = buffer->que_size - write_index;
+
 	sem_post(&buffer->read);
 		/*free lock*/
 	pthread_mutex_unlock(&buffer->write_in_use);
@@ -413,6 +458,30 @@ void* Consumer32(void* arg)
 
 /*==OVERFLOW, UNDERFLOW==*/
 /*1*/
+int OverUnderFlow1()
+{
+	pthread_t tid[20];
+	EX41_t buffer = {0, 0, 0, 0, {0}};
+	int i = 0;
+
+	if (pthread_mutex_init(&buffer.in_use_mutex, NULL))
+	{
+		perror("Mutex init failed");
+		return 1;
+	}
+
+	for (i = 0; i < 10; ++i)
+	{
+		pthread_create(&tid[i], NULL, Producer41, &buffer);
+		pthread_create(&tid[i+10], NULL, Consumer41, &buffer);
+	}
+	for (i = 0; i < 20; ++i)
+	{
+		pthread_join(tid[i], NULL);
+	}
+
+	return 0;
+}
 
 void* Producer41(void* arg)
 {
@@ -423,10 +492,11 @@ void* Producer41(void* arg)
 	pthread_mutex_lock(&buffer->in_use_mutex);
 
 		/*write to buffer*/
+	printf("Writer\n");
 
 		/*increase counter*/
 	__sync_fetch_and_add(&buffer->write, 1);
-
+	__sync_val_compare_and_swap(&buffer->write, QUE_SIZE, 0);
 		/*free lock*/
 	pthread_mutex_unlock(&buffer->in_use_mutex);
 
@@ -437,15 +507,20 @@ void* Consumer41(void* arg)
 {
 	EX41_t* buffer = arg;
 
+	/*check reader is not equal to writer*/
+	while (buffer->write == buffer->read)
+	{
+	}
 	/*check if lock is in use*/
 		/*set lock to in use*/
 	pthread_mutex_lock(&buffer->in_use_mutex);
 
 		/*write to buffer*/
+	printf("Reader\n");
 
 		/*decrease counter*/
 	__sync_fetch_and_add(&buffer->read, 1);
-
+	__sync_val_compare_and_swap(&buffer->read, QUE_SIZE, 0);
 
 		/*free lock*/
 	pthread_mutex_unlock(&buffer->in_use_mutex);
@@ -454,73 +529,171 @@ void* Consumer41(void* arg)
 }
 
 /*2*/
-typedef struct lock42
+int OverUnderFlow2()
 {
-	int write_index;
-	int read_index;
-	int write_in_use;
-	int read_in_use;
-	int *queue;
-}lock42_t;
+	pthread_t tid[20];
+	EX42_t buffer = {0, 0, 0, 0, {0}};
+	int i = 0;
 
-int Producer42(void)
-{
-	/*loop if lock has no writable space*/
-	/*check if not write_in_use*/
-		/*set write_in_use*/
-		/*write to queue*/
-		/*increase written*/
-		/*if written equals max queue space*/
-			/*set written back to 0*/
-		/*free lock*/
+	if (pthread_mutex_init(&buffer.write_in_use, NULL))
+	{
+		perror("Mutex init failed");
+		return 1;
+	}
+	if (pthread_mutex_init(&buffer.read_in_use, NULL))
+	{
+		perror("Mutex init failed");
+		return 1;
+	}
+
+	for (i = 0; i < 10; ++i)
+	{
+		pthread_create(&tid[i], NULL, Producer42, &buffer);
+		pthread_create(&tid[i+10], NULL, Consumer42, &buffer);
+	}
+	for (i = 0; i < 20; ++i)
+	{
+		pthread_join(tid[i], NULL);
+	}
+
 	return 0;
 }
 
-int Consumer42(void)
+void* Producer42(void* arg)
 {
-	/*loop if read is equal to written*/
+	EX42_t* buffer = arg;
+
+	/*check if not write_in_use*/
+		/*set write_in_use*/
+	pthread_mutex_lock(&buffer->write_in_use);
+
+		/*write to queue*/
+	printf("Writer\n");
+		/*increase written*/
+	buffer->write_index += 1;
+		/*if written equals max queue space*/
+			/*set written back to 0*/
+	if(QUE_SIZE <= buffer->write_index)
+	{
+		buffer->write_index = 0;
+	}
+		/*free lock*/
+	pthread_mutex_unlock(&buffer->write_in_use);
+
+	return NULL;
+}
+
+void* Consumer42(void* arg)
+{
+	EX42_t* buffer = arg;
+
 	/*check if not read_in_use*/
 		/*set read_in_use*/
+	pthread_mutex_lock(&buffer->read_in_use);
+	
+	while (buffer->write_index == buffer->read_index)
+	{
+	}
 		/*read from buffer*/
+	printf("Reader\n");
 		/*increase read*/
+	buffer->read_index += 1;
 		/*if read equals max queue space*/
 			/*set read back to zero*/
+	if(QUE_SIZE <= buffer->read_index)
+	{
+		buffer->read_index = 0;
+	}
 		/*free lock*/
-	return 0;
+	pthread_mutex_unlock(&buffer->read_in_use);
+
+	return NULL;
 }
 
 /*==BARRIER==*/
-typedef struct lock5
+int Barrier()
 {
-	int total_consumers;
-	int consumers_needed_to_read;
-	int in_use;
-	int have_read_array;
-	int buffer;
-}lock5_t;
+	pthread_t tid[10];
+	EX5_t buffer = {0, 0, 0, 0, 0, 0};
+	int i = 0;
 
-int Producer5(void)
-{
-	/*Loop until consumers need to read is zero*/
-	/*check if not in use*/
-		/*set in use*/
-		/*write to buffer*/
-		/*set conumers needed to read to the total number of consumers*/
-		/*set read array to zero*/
-		/*free lock*/
+	buffer.total_consumers = 10;
+
+	if (pthread_mutex_init(&buffer.in_use_mutex, NULL))
+	{
+		perror("Mutex init failed");
+		return 1;
+	}
+	if (pthread_cond_init(&buffer.barrier, NULL))
+	{
+		perror("cond init failed");
+		return 1;
+	}
+
+	for (i = 0; i < 10; ++i)
+	{
+		pthread_create(&tid[i], NULL, Consumer5, &buffer);
+	}
+	sleep(1);
+	Producer5(&buffer);
+
+	for (i = 0; i < 20; ++i)
+	{
+		pthread_join(tid[i], NULL);
+	}
+
 	return 0;
 }
 
-int Consumer5(void)
+void* Producer5(void* arg)
 {
-	/*loop infinately*/
-		/*if not read bit */
+	EX5_t* buffer = arg;
+
+	/*check if not in use*/
+		/*set in use*/
+	pthread_mutex_lock(&buffer->in_use_mutex);
+	while (buffer->consumers_needed_to_read)
+	{
+		pthread_cond_wait(&buffer->barrier, &buffer->in_use_mutex);
+	}
+		/*write to buffer*/
+	printf("Writer\n");
+
+		/*set conumers needed to read to the total number of consumers*/
+	buffer->consumers_needed_to_read = buffer->total_consumers;
+		/*set read array to zero*/
+	
+		/*free lock*/
+	pthread_cond_signal(&buffer->barrier);
+	pthread_mutex_unlock(&buffer->in_use_mutex);
+
+	return NULL;
+}
+
+void* Consumer5(void* arg)
+{
+	EX5_t* buffer = arg;
+
 			/*check if in use*/
 				/*set lock*/
+	pthread_mutex_lock(&buffer->in_use_mutex);
+	while (!buffer->consumers_needed_to_read /*&& thread has not read*/)
+	{
+		pthread_cond_wait(&buffer->barrier, &buffer->in_use_mutex);
+	}
 				/*read from buffer*/
+	printf("Reader\n");
 				/*decrease consumers need to read*/
+	buffer->consumers_needed_to_read -= 1;
 				/*if consumers need to read equals zero*/
 					/*decrease written*/
+	if (0 > buffer->consumers_needed_to_read)
+	{
+		buffer->consumers_needed_to_read = 0;
+	}
 				/*free lock*/
-	return 0;
+	pthread_cond_signal(&buffer->barrier);
+	pthread_mutex_unlock(&buffer->in_use_mutex);
+
+	return NULL;
 }
