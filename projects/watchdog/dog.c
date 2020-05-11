3076374cc562534881e2012ef1d10abc7b62afac
alistair.hudson <alistair.hudson64@gmail.com>
2020-05-11 15:52:31 +0300

    implementing watchdog code

---
+++ projects/watchdog/dog.c
@@ -0,0 +1,36 @@
+/******************************************************************************
+ *	Title:		Dog
+ *	Authour:	Alistair Hudson
+ *	Reviewer:	
+ *	Version:	11/05/2020.0
+ ******************************************************************************/
+
+#include "watchdog.h"
+
+/******MACROS******/
+#define ASSERT_NOT_NULL(ptr)	(assert(NULL != ptr))\
+
+/******TYPEDEFS, GLOBAL VARIABLES AND INTERNAL FUNCTIONS******/
+struct watchdog
+{
+
+};
+
+/******FUNCTIONS******/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- projects/watchdog/watchdog.c
+++ projects/watchdog/watchdog.c
@@ -11,28 +11,55 @@
 #define ASSERT_NOT_NULL(ptr)	(assert(NULL != ptr))\
 
 /******TYPEDEFS, GLOBAL VARIABLES AND INTERNAL FUNCTIONS******/
+struct watchdog
+{
+
+};
 
 /******FUNCTIONS******/
+/*This is to be placed in a separate .c file TODO*/
+int main (int argc, char* argv[])
+{
+/*set signal handler*/	
+	/*set and run watchdog*/
+	/*watchdog scheduler*/
+
+	return 0;
+}
+
 watchdog_t *WatchdogStart(char *program_name, char *arguments[])
 {
+	pid_t pid = 0;
 	/*create watchdog*/
-	/*create watchdog scheduler*/
-	/*create process scheduler*/
+	/*create dog watcher*/
 
 	/*fork the process to begin watchdog*/
-	/*if fork returns an error*/
-		/*perror*/
-		/*return null*/	
-	/*if child process*/
-		/*add sending task*/
-		/*add reeceeiving task*/
-		/*run watchdog*/
-
-	/*else*/
+	if (0 > (pid = fork()))
+	{
+		perror("Fork failed");
+		return NULL;
+	}	
+	if (0 == pid)
+	{
+		/*create watchdog scheduler*/
+		/*add signal sending task*/
+		/*add signal receiver task*/
+		/*semaphore to notify watchdog is read to run*/
+		if (0 > execv(args[0], args))
+		{
+			perror("Exec failed");
+			return NULL;
+		}		
+	}
+	else
+	{
+		/*set signal handler*/
 		/*add signal sending task*/
 		/*add signal receiving task*/
+		/* semaphore to wait for dog*/
 		/*create thread to run process scheduler*/
-		/*return watchdog*/
+		return /*watchdog*/
+	}
 }
 
 void WatchdogStop(watchdog_t *watchdog_instance)
@@ -41,10 +68,13 @@ void WatchdogStop(watchdog_t *watchdog_instance)
 	/*send stop to process scheduler*/
 
 	/*destroy watchdog scheduler*/
-	/*destropy process scheduler*/
+	/*destroy process scheduler*/
+
+	/*send process exit to watchdog*/
 
 }
 
+/*???????below  duplicate across to dog.c or can I use this just here??????*/
 static void* ProcessThreadScheduler(void* arg)
 {
 	/*run scheduler*/
@@ -63,10 +93,16 @@ static int SendSignal(void* other_process_id)
 
 static int IsAlive(void* arg)
 {
-	/*if not other process iis alive*/
+	if (/*not other_process_is_alive*/)
+	{
 		/*reboot process*/
+	}
+	/*process is alive set to 0*/
 	return 1;
 }
+
+
+
 
 
 
