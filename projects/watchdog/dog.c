2277a39ba6de9c23ab217638ed8d291d0caaa475
alistair.hudson <alistair.hudson64@gmail.com>
2020-05-11 16:10:35 +0300

    continued work on implementation

--- projects/watchdog/dog.c
+++ projects/watchdog/dog.c
@@ -5,18 +5,31 @@
  *	Version:	11/05/2020.0
  ******************************************************************************/
 
-#include "watchdog.h"
+#include "watchdog.c"
 
 /******MACROS******/
-#define ASSERT_NOT_NULL(ptr)	(assert(NULL != ptr))\
+#define ASSERT_NOT_NULL(ptr)	(assert(NULL != ptr))
 
 /******TYPEDEFS, GLOBAL VARIABLES AND INTERNAL FUNCTIONS******/
-struct watchdog
-{
-
-};
 
 /******FUNCTIONS******/
+int main (int argc, char* argv[])
+{
+/*set watchdog*/
+action_handler_t dog = {0};
+
+/*set signal handler*/	
+dog.sa_handler = IsAliveReceived;
+	if(0 > sigaction(SIGUSR1, &dog, NULL))
+	{
+		perror(“Sigaction failed”);
+		return 1;
+	}
+	/*run watchdog scheduler*/
+	SchedRun(watchdog_sched);
+
+	return 0;
+}
 
 
 
--- projects/watchdog/watchdog.c
+++ projects/watchdog/watchdog.c
@@ -8,7 +8,9 @@
 #include "watchdog.h"
 
 /******MACROS******/
-#define ASSERT_NOT_NULL(ptr)	(assert(NULL != ptr))\
+#define ASSERT_NOT_NULL(ptr)	(assert(NULL != ptr))
+#define SEND_TIME				(1)
+#define RECEIVE_TIME			(5)
 
 /******TYPEDEFS, GLOBAL VARIABLES AND INTERNAL FUNCTIONS******/
 struct watchdog
@@ -17,21 +19,21 @@ struct watchdog
 };
 
 /******FUNCTIONS******/
-/*This is to be placed in a separate .c file TODO*/
-int main (int argc, char* argv[])
-{
-/*set signal handler*/	
-	/*set and run watchdog*/
-	/*watchdog scheduler*/
-
-	return 0;
-}
 
 watchdog_t *WatchdogStart(char *program_name, char *arguments[])
 {
 	pid_t pid = 0;
+	action_handler_t watcher = {0};
 	/*create watchdog*/
 	/*create dog watcher*/
+
+	/*set signal handler for dog watcher*/
+	watcher.sa_handler = IsAliveReceived;
+	if(0 > sigaction(SIGUSR1, &dog, NULL))
+	{
+		perror(“Sigaction failed”);
+		return NULL;
+	}
 
 	/*fork the process to begin watchdog*/
 	if (0 > (pid = fork()))
@@ -42,9 +44,13 @@ watchdog_t *WatchdogStart(char *program_name, char *arguments[])
 	if (0 == pid)
 	{
 		/*create watchdog scheduler*/
-		/*add signal sending task*/
-		/*add signal receiver task*/
+		watchdog_sched = SchedCreate();
+/*add signal sending task*/
+		SchedAdd(watchdog_sched, SendSignal, watcher_id, SEND_TIME, 0);
+		/*add signal receiving task*/
+		SchedAdd(watcher_sched, IsAliveReceived, SIGUSR2, RECEIVE_TIME, 0);
 		/*semaphore to notify watchdog is read to run*/
+		sem_post(&dog_is_ready);
 		if (0 > execv(args[0], args))
 		{
 			perror("Exec failed");
@@ -53,11 +59,14 @@ watchdog_t *WatchdogStart(char *program_name, char *arguments[])
 	}
 	else
 	{
-		/*set signal handler*/
 		/*add signal sending task*/
+		SchedAdd(dog_watcher_sched, SendSignal, watchdog_id, SEND_TIME, 0);
 		/*add signal receiving task*/
+		SchedAdd(dog_watcher_sched, IsAliveReceived, SIGUSR1, RECEIVE_TIME, 0);
 		/* semaphore to wait for dog*/
+		sem_wait(&dog_is_ready);
 		/*create thread to run process scheduler*/
+		pthread_create(&watcher_thread, NULL, ProcessThreadScheduler, &dog_watch_sched);
 		return /*watchdog*/
 	}
 }
@@ -65,10 +74,14 @@ watchdog_t *WatchdogStart(char *program_name, char *arguments[])
 void WatchdogStop(watchdog_t *watchdog_instance)
 {
 	/*send stop to watchdog scheduler*/
+	SchedStop(watchdog_sched);
 	/*send stop to process scheduler*/
+	SchedStop(watch_sched);
 
 	/*destroy watchdog scheduler*/
+	SchedDestroy(watchdog_sched);
 	/*destroy process scheduler*/
+	SchedDestroy(watch_sched);
 
 	/*send process exit to watchdog*/
 
@@ -77,10 +90,10 @@ void WatchdogStop(watchdog_t *watchdog_instance)
 /*???????below  duplicate across to dog.c or can I use this just here??????*/
 static void* ProcessThreadScheduler(void* arg)
 {
-	/*run scheduler*/
+	SchedRun(dog_watch_sched);
 }
 
-static void SignalReceived(int signum)
+static void IsAliveReceived(int signum)
 {
 	oposite_process_is_alive = 1;
 }
@@ -88,19 +101,23 @@ static void SignalReceived(int signum)
 static int SendSignal(void* other_process_id)
 {
 	/*send signal to other process*/
+	if (0 > kill(other_process_id, signum))
+	{
+		perror(“Signal Error”);
+		return 0;
+	}
 	return 1;
 }
 
 static int IsAlive(void* arg)
 {
-	if (/*not other_process_is_alive*/)
+	if (!other_process_is_alive)
 	{
 		/*reboot process*/
 	}
 	/*process is alive set to 0*/
 	return 1;
 }
-
 
 
 
