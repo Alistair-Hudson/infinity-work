c1944f959f0c76f685f3f4bb225727a0d6d7a897
alistair.hudson <alistair.hudson64@gmail.com>
2020-06-10 11:09:35 +0300

    Turning shapes_test.cpp into a small game

--- cpp/test/shapes_test.cpp
+++ cpp/test/shapes_test.cpp
@@ -5,6 +5,7 @@
  *	Version:	08.06.2020.0
  ******************************************************************************/
 
+#include <iostream>
 
 #include "line.hpp"
 #include "square.hpp"
@@ -41,6 +42,7 @@ Circle circle(0, 0, 0, 100, COLOR_YELLOW);
 
 static void DrawFunc();
 static int KeyCapture(unsigned char key, int x, int y);
+static int TimedEvents();
 
 /*****FUNCTION DEFINITION******/
 
@@ -48,6 +50,7 @@ int main(int argc, char** argv)
 {
     DrawInit(argc, argv, SCREEN_WIDTH, SCREEN_HEIGHT, DrawFunc);
     DrawSetKeyboardFunc(KeyCapture);
+    DrawSetTimerFunc(TimedEvents, 100);
 
     DrawMainLoop();
     
@@ -58,38 +61,69 @@ static void DrawFunc()
 {
     target_horiz.DrawInternal();
     target_vert.DrawInternal();
+    square.DrawInternal();
 }
 
 static int KeyCapture(unsigned char key, int x, int y)
 {
-    Point up_vector(0, 1);
-    Point down_vector(0, -1);
-    Point left_vector(-1, 0);
-    Point right_vector(1, 0);
+    Point up_vector(0, -5);
+    Point down_vector(0, 5);
+    Point left_vector(-5, 0);
+    Point right_vector(5, 0);
+    Point reset(rand()%SCREEN_WIDTH, rand()%SCREEN_HEIGHT);
 
     switch (key)
     {
     case 27: //ESC key Capture
         return -1;
     
-    case 38: //Up arrow key caputre
+    case 'w': //W key caputre
         target_horiz.Move(up_vector);
         target_vert.Move(up_vector);
         break;
-    case 40: //Up arrow key caputre
-        target_horiz.Move(up_vector);
-        target_vert.Move(up_vector);
+    case 's': //S key caputre
+        target_horiz.Move(down_vector);
+        target_vert.Move(down_vector);
         break;
-    case 37: //Left arrow key caputre
+    case 'a': //A key caputre
         target_horiz.Move(left_vector);
         target_vert.Move(left_vector);
         break;
-    case 39: //Right arrow key caputre
+    case 'd': //D key caputre
         target_horiz.Move(right_vector);
         target_vert.Move(right_vector);
+        break;
+    case ' ': // Space key cpature
+        if (target_horiz.GetPos().GetX()-square.GetPos().GetX() < square.GetLength()/2 && 
+            target_horiz.GetPos().GetX()-square.GetPos().GetX() > -square.GetLength()/2 && 
+            target_horiz.GetPos().GetY()-square.GetPos().GetY() < square.GetLength()/2 && 
+            target_horiz.GetPos().GetY()-square.GetPos().GetY() > -square.GetLength()/2)
+        {
+            square.SetPos(reset);
+        }
         break;
     default:
         break;
     }
     return 0;
+}
+
+static int TimedEvents()
+{
+    int vert_dir = (rand()%2 == 0) ? 1 : -1;
+    int horiz_dir = (rand()%2 == 0) ? 1 : -1;
+
+    Point vector(1, 1);//(horiz_dir*(rand()%5), vert_dir*(rand()%5));
+    Point reset(rand()%SCREEN_WIDTH, rand()%SCREEN_HEIGHT);
+
+    square.Move(vector);
+    if (square.GetPos().GetX() > SCREEN_WIDTH ||
+        square.GetPos().GetX() < 0 ||
+        square.GetPos().GetY() > SCREEN_HEIGHT ||
+        square.GetPos().GetY() > 0)
+    {
+        square.SetPos(reset);
+    }
+
+    return 1;
 }
