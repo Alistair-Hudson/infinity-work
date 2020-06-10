1b4a8013b2ee260341b1dd9648e4c5f4b697b3d2
alistair.hudson <alistair.hudson64@gmail.com>
2020-06-10 09:41:28 +0300

    Working on shape test file

--- cpp/test/shapes_test.cpp
+++ cpp/test/shapes_test.cpp
@@ -19,6 +19,18 @@
 
 /****** GLOBAL VARIABLES*****/
 
+/*===TARGETING RECTICLE===*/
+Line target_vert(SCREEN_WIDTH/2, 
+                SCREEN_HEIGHT/2, 
+                90, 100, COLOR_RED);
+Line target_horiz(SCREEN_WIDTH/2, 
+                SCREEN_HEIGHT/2, 
+                0, 100, COLOR_RED);
+
+/*===SHAPE TARGETS===*/
+Square square(0, 0, 0, 100, COLOR_GREEN);
+Circle circle(0, 0, 0, 100, COLOR_YELLOW);
+
 /*****STRUCTS*******/
 
 /*****CLASSES******/
@@ -28,18 +40,56 @@
 /******INTERNAL FUNCTION DECLARATION******/
 
 static void DrawFunc();
+static int KeyCapture(unsigned char key, int x, int y);
 
 /*****FUNCTION DEFINITION******/
 
 int main(int argc, char** argv)
 {
     DrawInit(argc, argv, SCREEN_WIDTH, SCREEN_HEIGHT, DrawFunc);
+    DrawSetKeyboardFunc(KeyCapture);
 
+    DrawMainLoop();
     
     return 0;
 }
 
 static void DrawFunc()
 {
+    target_horiz.DrawInternal();
+    target_vert.DrawInternal();
+}
 
+static int KeyCapture(unsigned char key, int x, int y)
+{
+    Point up_vector(0, 1);
+    Point down_vector(0, -1);
+    Point left_vector(-1, 0);
+    Point right_vector(1, 0);
+
+    switch (key)
+    {
+    case 27: //ESC key Capture
+        return -1;
+    
+    case 38: //Up arrow key caputre
+        target_horiz.Move(up_vector);
+        target_vert.Move(up_vector);
+        break;
+    case 40: //Up arrow key caputre
+        target_horiz.Move(up_vector);
+        target_vert.Move(up_vector);
+        break;
+    case 37: //Left arrow key caputre
+        target_horiz.Move(left_vector);
+        target_vert.Move(left_vector);
+        break;
+    case 39: //Right arrow key caputre
+        target_horiz.Move(right_vector);
+        target_vert.Move(right_vector);
+        break;
+    default:
+        break;
+    }
+    return 0;
 }
