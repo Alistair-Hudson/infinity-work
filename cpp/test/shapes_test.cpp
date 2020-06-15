d5783cdcb988d1345c8bbb05df67a3c85fbd6e1a
alistair.hudson <alistair.hudson64@gmail.com>
2020-06-15 08:52:43 +0300

    working on game

--- cpp/test/shapes_test.cpp
+++ cpp/test/shapes_test.cpp
@@ -6,6 +6,8 @@
  ******************************************************************************/
 
 #include <iostream>
+#include <list>
+#include <iterator>
 
 #include "line.hpp"
 #include "square.hpp"
@@ -21,186 +23,8 @@
 /*****STRUCTS*******/
 
 /*****CLASSES******/
-class Node
-{
-public:
-    Node();
-    Node(const Node& node_);
-    Node& operator= (const Node& node_);
-    ~Node();
-
-    inline Square GetShape() const;
-    inline Node* NextNode() const;
-    inline void SetNextNode(Node* node_);
-    inline void SetShape(Square shape_);
-
-private:
-    Square shape;
-    Node* next_node;
-};
-
-class Targets
-{
-public:
-    Targets(int x_);
-    Targets(const Targets& targets_);
-    Targets& operator=(const Targets& targets_);
-    ~Targets();
-
-    void InsertNode(Square& shape);
-    void RemoveNode(Node& node);
-    inline Node* Begining() const;
-    inline Node* End() const;
-    void DrawAll();
-    void MoveAll();
-    void CheckAll(Line& target);
-
-private:
-    Node* head_node;
-    Node* tail_node;
-};
 
 /******CLASS FUNCTIONS*******/
-/*===NODE FUNCTIONS===*/
-Node::Node()
-{
-    next_node = NULL;
-}
-
-Node::Node(const Node& node_)
-{
-    shape = node_.GetShape();
-    next_node = node_.NextNode();
-}
-
-Node& Node::operator=(const Node& node_)
-{
-    shape = node_.GetShape();
-    next_node = node_.NextNode();
-    return *this;
-}
-
-Node::~Node()
-{
-
-}
-
-Square Node::GetShape() const
-{
-    return shape;
-}
-
-Node* Node::NextNode() const
-{
-    return next_node;
-}
-
-void Node::SetNextNode(Node* node_)
-{
-    next_node = node_;
-}
-
-void Node::SetShape(Square shape_)
-{
-    shape = shape_;
-}
-
-/*===Targets Functions===*/
-Targets::Targets(int x_)
-{
-    head_node = new Node;
-    tail_node = head_node;
-}
-
-Targets::Targets(const Targets& targets_)
-{
-    head_node = targets_.Begining();
-    tail_node = targets_.End();
-}
-
-Targets& Targets::operator=(const Targets& targets_)
-{
-    head_node = targets_.Begining();
-    tail_node = targets_.End();
-    return *this;
-}
-
-Targets::~Targets()
-{
-    delete tail_node;
-}
-
-void Targets::InsertNode(Square& shape)
-{
-    Node *new_node = new Node;
-    End()->SetShape(shape);
-    End()->SetNextNode(new_node);
-    tail_node = new_node;
-
-}
-
-void Targets::RemoveNode(Node& node)
-{
-    Node *next_node = node.NextNode();
-    next_node->SetNextNode(next_node);
-    next_node->SetShape(next_node->GetShape());
-    delete next_node;
-}
-
-Node* Targets::Begining() const
-{
-    return head_node;
-}
-
-Node* Targets::End() const
-{
-    return tail_node;
-}
-
-void Targets::DrawAll()
-{
-    Node* node =  Begining();
-    while(node != End())
-    {
-        node->GetShape().DrawInternal();
-        node = node->NextNode();
-    }
-}
-
-void Targets::MoveAll()
-{
-    Node* node = Begining();
-    Point vector(0,0);
-
-    while(node != End())
-    {
-        vector.SetX((rand()%5)-10);
-        vector.SetY(rand()%5);
-        node->GetShape().Move(vector);
-        //std::cout << node->GetShape().GetPos().GetX() << ", " << node->GetShape().GetPos().GetY() << std::endl;
-        node = node->NextNode();
-    }
-    
-}
-
-void Targets::CheckAll(Line& target)
-{
-    Node* node = Begining();
-    Point reset(0, 0);
-
-    while (node != End())
-    {
-        if (target.GetPos().GetX()-node->GetShape().GetPos().GetX() < node->GetShape().GetLength()/2 && 
-            target.GetPos().GetX()-node->GetShape().GetPos().GetX() > -node->GetShape().GetLength()/2 && 
-            target.GetPos().GetY()-node->GetShape().GetPos().GetY() < node->GetShape().GetLength()/2 && 
-            target.GetPos().GetY()-node->GetShape().GetPos().GetY() > -node->GetShape().GetLength()/2)
-        {
-            reset.SetX(rand()%SCREEN_WIDTH);
-            reset.SetY(0);
-            node->GetShape().SetPos(reset);
-        }
-    }
-}
 
 /****** GLOBAL VARIABLES*****/
 
@@ -213,23 +37,30 @@ Line target_horiz(SCREEN_WIDTH/2,
                 0, 100, COLOR_RED);
 
 /*===SHAPE TARGETS===*/
-Square square(0, 0, 0, 100, COLOR_GREEN);
+Square square(0, 0, 0, 100, COLOR_YELLOW);
 Circle circle(0, 0, 0, 100, COLOR_YELLOW);
-Targets target_list(0);
+Square square2(0, 0, 0, 100, COLOR_GREEN);
+std::list<Square> target_list;
 
 /******INTERNAL FUNCTION DECLARATION******/
 
 static void DrawFunc();
 static int KeyCapture(unsigned char key, int x, int y);
 static int TimedEvents();
-
-bool operator!=(const Node& node1, const Node& node2);
+static void DrawAll();
+static void MoveAll();
+static void CheckAll();
+static void AddTarget();
 
 /*****FUNCTION DEFINITION******/
 
 int main(int argc, char** argv)
 {
-    target_list.InsertNode(square);
+    target_list.push_back(square);
+    target_list.push_back(square);
+    target_list.push_back(square);
+    target_list.push_back(square2);
+
 
     DrawInit(argc, argv, SCREEN_WIDTH, SCREEN_HEIGHT, DrawFunc);
     DrawSetKeyboardFunc(KeyCapture);
@@ -244,7 +75,7 @@ static void DrawFunc()
 {
     target_horiz.DrawInternal();
     target_vert.DrawInternal();
-    target_list.DrawAll();
+    DrawAll();
 }
 
 static int KeyCapture(unsigned char key, int x, int y)
@@ -277,7 +108,7 @@ static int KeyCapture(unsigned char key, int x, int y)
         target_vert.Move(right_vector);
         break;
     case ' ': // Space key cpature
-        target_list.CheckAll(target_horiz);
+        CheckAll();
         break;
     default:
         break;
@@ -287,27 +118,66 @@ static int KeyCapture(unsigned char key, int x, int y)
 
 static int TimedEvents()
 {
-    int vert_dir = (rand()%2 == 0) ? 1 : -1;
-    int horiz_dir = (rand()%2 == 0) ? 1 : -1;
-
-    Point vector(1, 1);//(horiz_dir*(rand()%5), vert_dir*(rand()%5));
-    Point reset(0, 0); //(rand()%SCREEN_WIDTH, rand()%SCREEN_HEIGHT);
-
-    target_list.MoveAll();
-    /*
-    if (square.GetPos().GetX() > SCREEN_WIDTH ||
-        square.GetPos().GetX() < 0 ||
-        square.GetPos().GetY() > SCREEN_HEIGHT ||
-        square.GetPos().GetY() > 0)
-    {
-        square.SetPos(reset);
-    // }*/
-
+    MoveAll();
+    AddTarget();
     return 1;
 }
 
-bool operator!=(const Node& node1, const Node& node2)
+static void DrawAll()
 {
-    return node1!=node2;
+    std::list<Square>::iterator iter = target_list.begin();
+
+    while(iter != target_list.end())
+    {
+       iter->DrawInternal();
+       iter = next(iter, 1);
+    }
 }
 
+void MoveAll()
+{
+    std::list<Square>::iterator iter = target_list.begin();
+    Point vector(0, 0);
+
+    while(iter != target_list.end())
+    {
+        vector.SetX(5-(rand()%10));
+        vector.SetY(rand()%5);
+        iter->Move(vector);
+        iter = next(iter, 1);
+    } 
+}
+
+static void CheckAll()
+{
+    std::list<Square>::iterator iter = target_list.begin();
+    Point reset(0, 0);
+
+    while (iter != target_list.end())
+    {
+        if (target_horiz.GetPos().GetX()-iter->GetPos().GetX() < iter->GetLength()/2 && 
+            target_horiz.GetPos().GetX()-iter->GetPos().GetX() > -iter->GetLength()/2 && 
+            target_horiz.GetPos().GetY()-iter->GetPos().GetY() < iter->GetLength()/2 && 
+            target_horiz.GetPos().GetY()-iter->GetPos().GetY() > -iter->GetLength()/2)
+        {
+            reset.SetX(rand()%SCREEN_WIDTH);
+            iter->SetPos(reset);
+        }
+        iter = next(iter, 1);
+    }
+}
+
+static void AddTarget()
+{
+    Point start_pos(0, 0);
+    switch(rand()%50)
+    {
+        case (5):
+            start_pos.SetX(rand()%SCREEN_WIDTH);
+            target_list.push_back(square);
+            target_list.end()->SetPos(start_pos);
+            break;
+        default:
+            break;
+    }
+}
